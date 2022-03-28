89716e29f237cd6d3ba2787397a06431cbb23f29
SankethBK <sankethbk7777@gmail.com>
2022-03-29 01:28:38 +0530

    Added tests for AuthSession bloc and fixed bugs in AuthSession Bloc

--- lib/features/auth/presentation/bloc/auth_session/auth_session_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_session/auth_session_bloc.dart
@@ -1,7 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'dart:async';
 import 'package:dairy_app/features/auth/domain/entities/logged_in_user.dart';
-import 'package:dairy_app/features/auth/domain/repositories/authentication_repository.dart';
 import 'package:equatable/equatable.dart';
 
 part 'auth_session_event.dart';
@@ -9,13 +7,8 @@ part 'auth_session_state.dart';
 
 class AuthSessionBloc extends Bloc<AuthSessionEvent, AuthSessionState> {
   AuthSessionBloc() : super(Unauthenticated()) {
-    on<AuthSessionEvent>(
-      (event, emit) {
-        on<UserLoggedIn>(
-            (event, emit) => emit(Authenticated(user: event.user)));
-        on<UserLoggedOut>((event, emit) => emit(Unauthenticated()));
-        on<AppLostFocus>(((event, emit) => Unauthenticated()));
-      },
-    );
+    on<UserLoggedIn>((event, emit) => emit(Authenticated(user: event.user)));
+    on<UserLoggedOut>((event, emit) => emit(Unauthenticated()));
+    on<AppLostFocus>(((event, emit) => emit(Unauthenticated())));
   }
 }
---
+++ test/features/auth/presentation/bloc/auth_session/auth_session_test.dart
@@ -0,0 +1,43 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dairy_app/features/auth/domain/entities/logged_in_user.dart';
+import 'package:dairy_app/features/auth/presentation/bloc/auth_session/auth_session_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late AuthSessionBloc authSessionBloc;
+  const LoggedInUser user = LoggedInUser(email: "sank@email.com", id: "77");
+
+  setUp(() {
+    authSessionBloc = AuthSessionBloc();
+  });
+
+  group("Testing AuthSessionBloc", () {
+    test("Initial state should be unauthenticated", () {
+      expect(authSessionBloc.state, equals(Unauthenticated()));
+    });
+
+    blocTest<AuthSessionBloc, AuthSessionState>(
+      'emits [Authenticated] when MyEvent is added.',
+      build: () => authSessionBloc,
+      act: (bloc) => (bloc).add(const UserLoggedIn(user: user)),
+      expect: () => <AuthSessionState>[const Authenticated(user: user)],
+    );
+
+    blocTest<AuthSessionBloc, AuthSessionState>(
+      'emits [Authenticated, Unauthenticated, Authenticated, Unauthenticated] when [UserLoggedIn, AppLostFocus, UserLoggedIn, UserLoggedOut] is added.',
+      build: () => authSessionBloc,
+      act: (bloc) {
+        bloc.add(const UserLoggedIn(user: user));
+        bloc.add(AppLostFocus());
+        bloc.add(const UserLoggedIn(user: user));
+        bloc.add(UserLoggedOut());
+      },
+      expect: () => <AuthSessionState>[
+        const Authenticated(user: user),
+        Unauthenticated(),
+        const Authenticated(user: user),
+        Unauthenticated(),
+      ],
+    );
+  });
+}
