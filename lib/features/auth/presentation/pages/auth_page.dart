a8d9454c9f29f2be21be3b422aa82c67178b3862
SankethBK <sanketh.bk@razorpay.com>
2022-06-30 09:39:28 +0530

    Fixed app close

--- lib/features/auth/presentation/pages/auth_page.dart
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -3,9 +3,11 @@
 import 'package:dairy_app/core/animations/flip_card_animation.dart';
 import 'package:dairy_app/core/dependency_injection/injection_container.dart';
 import 'package:dairy_app/features/auth/data/repositories/fingerprint_auth_repo.dart';
+import 'package:dairy_app/features/auth/presentation/widgets/quit_app_dialog.dart';
 import 'package:dairy_app/features/auth/presentation/widgets/sign_in_form.dart';
 import 'package:dairy_app/features/auth/presentation/widgets/sign_up_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthPage extends StatefulWidget {
   // user id of last logged in user to determine if it is a fresh login or not
@@ -39,42 +41,52 @@ class _AuthPageState extends State<AuthPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              "assets/images/digital-art-neon-bubbles.jpg",
+    return WillPopScope(
+      onWillPop: () async {
+        bool res = await quitAppDialog(context);
+        if (res == true) {
+          SystemNavigator.pop();
+        }
+
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/images/digital-art-neon-bubbles.jpg",
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
-        ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FlipCardAnimation(
-                  frontWidget: (void Function() flipCard) {
-                    return SignUpForm(flipCard: flipCard);
-                  },
-                  rearWidget: (void Function() flipCard) {
-                    return SignInForm(
-                        flipCard: flipCard,
-                        lastLoggedInUserId: widget.lastLoggedInUserId);
-                  },
-                ),
-                const SizedBox(height: 40),
-                if (widget.fingerPrintAuthRepository
-                        .shouldActivateFingerPrint() &&
-                    MediaQuery.of(context).viewInsets.bottom == 0)
-                  Icon(
-                    Icons.fingerprint,
-                    size: 50,
-                    color: Colors.white.withOpacity(0.5),
-                  )
-              ],
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlipCardAnimation(
+                    frontWidget: (void Function() flipCard) {
+                      return SignUpForm(flipCard: flipCard);
+                    },
+                    rearWidget: (void Function() flipCard) {
+                      return SignInForm(
+                          flipCard: flipCard,
+                          lastLoggedInUserId: widget.lastLoggedInUserId);
+                    },
+                  ),
+                  const SizedBox(height: 40),
+                  if (widget.fingerPrintAuthRepository
+                          .shouldActivateFingerPrint() &&
+                      MediaQuery.of(context).viewInsets.bottom == 0)
+                    Icon(
+                      Icons.fingerprint,
+                      size: 50,
+                      color: Colors.white.withOpacity(0.5),
+                    )
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/features/auth/presentation/widgets/quit_app_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:dairy_app/core/widgets/cancel_button.dart';
+import 'package:dairy_app/core/widgets/glass_dialog.dart';
+import 'package:dairy_app/core/widgets/submit_button.dart';
+import 'package:flutter/material.dart';
+
+Future<bool> quitAppDialog(BuildContext context) async {
+  return await showCustomDialog(
+    context: context,
+    child: Container(
+      padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Text(
+            "Close the App?",
+            style: TextStyle(fontSize: 18.0),
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CancelButton(
+                buttonText: "No",
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+              const SizedBox(width: 10),
+              SubmitButton(
+                isLoading: false,
+                onSubmitted: () => Navigator.pop(context, true),
+                buttonText: "Yes",
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
