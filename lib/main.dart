95f4fd18cedbbcd217794e56ac59b5d7de80030f
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-07-12 18:08:30 +0530

    Disabled landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dairy_app/app/bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +12,9 @@ Future<void> main() async {
   GoogleFonts.config.allowRuntimeFetching = false;
   return BlocOverrides.runZoned(
     () async {
+      SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+      );
       WidgetsFlutterBinding.ensureInitialized();
       await Firebase.initializeApp();
       await di.init();
