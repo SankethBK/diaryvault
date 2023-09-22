907ea57b4f2aee990d171c0c39275c371c271da9
Sanketh B K <sanketh.bk@razorpay.com>
2023-09-23 01:01:46 +0530

    added privacy policy

--- lib/features/auth/presentation/pages/auth_page.dart
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -3,14 +3,17 @@
 import 'package:dairy_app/app/themes/theme_extensions/auth_page_theme_extensions.dart';
 import 'package:dairy_app/core/animations/flip_card_animation.dart';
 import 'package:dairy_app/core/dependency_injection/injection_container.dart';
+import 'package:dairy_app/core/widgets/glassmorphism_cover.dart';
 import 'package:dairy_app/features/auth/data/repositories/fingerprint_auth_repo.dart';
 import 'package:dairy_app/features/auth/presentation/bloc/auth_session/auth_session_bloc.dart';
+import 'package:dairy_app/features/auth/presentation/widgets/privacy_policy.dart';
 import 'package:dairy_app/features/auth/presentation/widgets/quit_app_dialog.dart';
 import 'package:dairy_app/features/auth/presentation/widgets/sign_in_form.dart';
 import 'package:dairy_app/features/auth/presentation/widgets/sign_up_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AuthPage extends StatefulWidget {
   // user id of last logged in user to determine if it is a fresh login or not
@@ -60,6 +63,9 @@ class _AuthPageState extends State<AuthPage> {
   Widget build(BuildContext context) {
     final backgroundImagePath =
         Theme.of(context).extension<AuthPageThemeExtensions>()!.backgroundImage;
+
+    final linkColor =
+        Theme.of(context).extension<AuthPageThemeExtensions>()!.linkColor;
     return WillPopScope(
       onWillPop: () async {
         bool res = await quitAppDialog(context);
@@ -71,53 +77,63 @@ class _AuthPageState extends State<AuthPage> {
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Container(
-          constraints: BoxConstraints.expand(),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                backgroundImagePath,
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FlipCardAnimation(
-                    frontWidget: (void Function() flipCard) {
-                      return SignUpForm(
-                        flipCard: flipCard,
-                      );
-                    },
-                    rearWidget: (void Function() flipCard) {
-                      return SignInForm(
-                          flipCard: flipCard,
-                          lastLoggedInUserId: widget.lastLoggedInUserId);
-                    },
+        body: Stack(
+          children: [
+            Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    backgroundImagePath,
                   ),
-                  const SizedBox(height: 40),
-                  if (widget.fingerPrintAuthRepository
-                          .shouldActivateFingerPrint() &&
-                      MediaQuery.of(context).viewInsets.bottom == 0)
-                    Icon(
-                      Icons.fingerprint,
-                      size: 50,
-                      color: Colors.white.withOpacity(0.5),
-                    ),
-                  SizedBox(
-                    height: MediaQuery.of(context).viewInsets.bottom,
-                  )
-                ],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FlipCardAnimation(
+                        frontWidget: (void Function() flipCard) {
+                          return SignUpForm(
+                            flipCard: flipCard,
+                          );
+                        },
+                        rearWidget: (void Function() flipCard) {
+                          return SignInForm(
+                              flipCard: flipCard,
+                              lastLoggedInUserId: widget.lastLoggedInUserId);
+                        },
+                      ),
+                      const SizedBox(height: 40),
+                      if (widget.fingerPrintAuthRepository
+                              .shouldActivateFingerPrint() &&
+                          MediaQuery.of(context).viewInsets.bottom == 0)
+                        Icon(
+                          Icons.fingerprint,
+                          size: 50,
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      SizedBox(
+                        height: MediaQuery.of(context).viewInsets.bottom,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
+            Positioned(
+              child: Center(
+                child: PrivacyPolicy(linkColor: linkColor),
+              ),
+              bottom: 20,
+              right: 0,
+              left: 0,
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// shdjdjeui
