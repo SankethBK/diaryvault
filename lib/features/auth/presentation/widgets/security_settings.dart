d8a569758b1cee82cb6e1206d102cd9900d4e93c
Sameer Kumar <kumar.sam1612@gmail.com>
2023-10-02 20:42:35 +0530

    #39 - added accordion for security settings. (#48)

--- lib/features/auth/presentation/widgets/security_settings.dart
+++ lib/features/auth/presentation/widgets/security_settings.dart
@@ -12,11 +12,14 @@ import 'package:dairy_app/features/auth/presentation/widgets/password_enter_popu
 import 'package:dairy_app/features/auth/presentation/widgets/password_reset_popup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_accordion/widgets/AccordionHeaderItem.dart';
+import 'package:simple_accordion/widgets/AccordionItem.dart';
+import 'package:simple_accordion/widgets/AccordionWidget.dart';
 
 // ignore: must_be_immutable
 class SecuritySettings extends StatelessWidget {
   late IAuthenticationRepository authenticationRepository;
+
   SecuritySettings({Key? key}) : super(key: key) {
     authenticationRepository = sl<IAuthenticationRepository>();
   }
@@ -43,140 +46,160 @@ class SecuritySettings extends StatelessWidget {
             state.userConfigModel!.isFingerPrintLoginEnabled;
 
         final userId = state.userConfigModel?.userId;
-        return Container(
-          padding: const EdgeInsets.only(top: 5.0),
-          width: double.infinity,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Material(
-                color: Colors.transparent,
-                child: SettingsTile(
-                  child: Text(
-                    "Change password",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: mainTextColor,
-                    ),
-                  ),
-                  onTap: () async {
-                    if (userId == null) {
-                      showToast("Unexpected error occured");
-                      return;
-                    }
-
-                    if (userId == GuestUserDetails.guestUserId) {
-                      showToast(
-                          "Please setup your account to use this feature");
-                      return;
-                    }
-                    String? result = await passwordLoginPopup(
-                      context: context,
-                      submitPassword: (password) => authenticationRepository
-                          .verifyPassword(userId!, password),
-                    );
-
-                    // old password will be retrieved from previous dialog
-                    if (result != null) {
-                      passwordResetPopup(
-                        context: context,
-                        submitPassword: (newPassword) =>
-                            authenticationRepository.updatePassword(
-                          authSessionBloc.state.user!.email,
-                          result,
-                          newPassword,
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              Material(
-                color: Colors.transparent,
-                child: SettingsTile(
-                  child: Text(
-                    "Change email",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: mainTextColor,
-                    ),
-                  ),
-                  onTap: () async {
-                    if (userId == null) {
-                      showToast("Unexpected error occured");
-                    }
-
-                    if (userId == GuestUserDetails.guestUserId) {
-                      showToast(
-                          "Please setup your account to use this feature");
-                    }
-
-                    String? result = await passwordLoginPopup(
-                      context: context,
-                      submitPassword: (password) =>
-                          authenticationRepository.verifyPassword(
-                              authSessionBloc.state.user!.id, password),
-                    );
-
-                    // old password will be retrieved from previous dialog
-                    var emailChanged;
-                    if (result != null) {
-                      emailChanged = await emailChangePopup(
-                        context,
-                        (newEmail) => authenticationRepository.updateEmail(
-                          oldEmail: authSessionBloc.state.user!.email,
-                          password: result,
-                          newEmail: newEmail,
-                        ),
-                      );
-                    }
-
-                    if (emailChanged == true) {
-                      authSessionBloc.add(UserLoggedOut());
-                    }
-                  },
-                ),
-              ),
-              const SizedBox(height: 10),
-              SwitchListTile(
-                inactiveTrackColor: inactiveTrackColor,
-                activeColor: activeColor,
-                contentPadding: const EdgeInsets.all(0.0),
-                title: Text(
-                  "Enable fingerprint login",
-                  style: TextStyle(color: mainTextColor),
-                ),
-                subtitle: Text(
-                  "Fingerprint auth should be enabled in device settings",
-                  style: TextStyle(color: mainTextColor),
-                ),
-                value: isFingerPrintLoginEnabledValue ?? false,
-                onChanged: (value) async {
-                  if (userId == null) {
-                    showToast("Unexpected error occured");
-                    return;
-                  }
-
-                  if (userId == GuestUserDetails.guestUserId) {
-                    showToast("Please setup your account to use this feature");
-                    return;
-                  }
-
-                  try {
-                    await authenticationRepository.isFingerprintAuthPossible();
-                    userConfigCubit.setUserConfig(
-                      UserConfigConstants.isFingerPrintLoginEnabled,
-                      value,
-                    );
-                  } on Exception catch (e) {
-                    showToast(e.toString().replaceAll("Exception: ", ""));
-                  }
-                },
-              ),
-            ],
+        return SimpleAccordion(
+          headerTextStyle: TextStyle(
+            color: mainTextColor,
+            fontSize: 16,
           ),
+          children: [
+            AccordionHeaderItem(
+              title: "Security Settings",
+              children: [
+                AccordionItem(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Material(
+                          color: Colors.transparent,
+                          child: SettingsTile(
+                            child: Text(
+                              "Change password",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: mainTextColor,
+                              ),
+                            ),
+                            onTap: () async {
+                              if (userId == null) {
+                                showToast("Unexpected error occured");
+                                return;
+                              }
+
+                              if (userId == GuestUserDetails.guestUserId) {
+                                showToast(
+                                    "Please setup your account to use this feature");
+                                return;
+                              }
+                              String? result = await passwordLoginPopup(
+                                context: context,
+                                submitPassword: (password) =>
+                                    authenticationRepository.verifyPassword(
+                                        userId!, password),
+                              );
+
+                              // old password will be retrieved from previous dialog
+                              if (result != null) {
+                                passwordResetPopup(
+                                  context: context,
+                                  submitPassword: (newPassword) =>
+                                      authenticationRepository.updatePassword(
+                                    authSessionBloc.state.user!.email,
+                                    result,
+                                    newPassword,
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Material(
+                          color: Colors.transparent,
+                          child: SettingsTile(
+                            child: Text(
+                              "Change email",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: mainTextColor,
+                              ),
+                            ),
+                            onTap: () async {
+                              if (userId == null) {
+                                showToast("Unexpected error occured");
+                              }
+
+                              if (userId == GuestUserDetails.guestUserId) {
+                                showToast(
+                                    "Please setup your account to use this feature");
+                              }
+
+                              String? result = await passwordLoginPopup(
+                                context: context,
+                                submitPassword: (password) =>
+                                    authenticationRepository.verifyPassword(
+                                        authSessionBloc.state.user!.id,
+                                        password),
+                              );
+
+                              // old password will be retrieved from previous dialog
+                              var emailChanged;
+                              if (result != null) {
+                                emailChanged = await emailChangePopup(
+                                  context,
+                                  (newEmail) =>
+                                      authenticationRepository.updateEmail(
+                                    oldEmail: authSessionBloc.state.user!.email,
+                                    password: result,
+                                    newEmail: newEmail,
+                                  ),
+                                );
+                              }
+
+                              if (emailChanged == true) {
+                                authSessionBloc.add(UserLoggedOut());
+                              }
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        SwitchListTile(
+                          inactiveTrackColor: inactiveTrackColor,
+                          activeColor: activeColor,
+                          contentPadding: const EdgeInsets.all(0.0),
+                          title: Text(
+                            "Enable fingerprint login",
+                            style: TextStyle(color: mainTextColor),
+                          ),
+                          subtitle: Text(
+                            "Fingerprint auth should be enabled in device settings",
+                            style: TextStyle(color: mainTextColor),
+                          ),
+                          value: isFingerPrintLoginEnabledValue ?? false,
+                          onChanged: (value) async {
+                            if (userId == null) {
+                              showToast("Unexpected error occured");
+                              return;
+                            }
+
+                            if (userId == GuestUserDetails.guestUserId) {
+                              showToast(
+                                  "Please setup your account to use this feature");
+                              return;
+                            }
+
+                            try {
+                              await authenticationRepository
+                                  .isFingerprintAuthPossible();
+                              userConfigCubit.setUserConfig(
+                                UserConfigConstants.isFingerPrintLoginEnabled,
+                                value,
+                              );
+                            } on Exception catch (e) {
+                              showToast(
+                                  e.toString().replaceAll("Exception: ", ""));
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         );
       },
     );
