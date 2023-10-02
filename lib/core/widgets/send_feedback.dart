ceade7da787c750b80196e3018df678933e0deec
Aku <micedreams@micedreams.domain.name>
2023-10-02 10:25:59 +0530

    Add new send feedback widget in settings page.

---
+++ lib/core/widgets/send_feedback.dart
@@ -0,0 +1,35 @@
+import 'package:dairy_app/app/themes/theme_extensions/note_create_page_theme_extensions.dart';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SendFeedBack extends StatelessWidget {
+  const SendFeedBack({Key? key}) : super(key: key);
+
+  final emailAddress = 'diaryvault.app@gmail.com';
+  final subject = 'Feedback for DiaryVault';
+
+  @override
+  Widget build(BuildContext context) => InkWell(
+        onTap: _launchEmailApp,
+        child: Text(
+          "Send feedback",
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Theme.of(context)
+                .extension<NoteCreatePageThemeExtensions>()!
+                .mainTextColor,
+          ),
+        ),
+      );
+
+  void _launchEmailApp() async {
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: emailAddress,
+      queryParameters: {'subject': subject},
+    );
+
+    await launchUrl(emailLaunchUri);
+  }
+}
