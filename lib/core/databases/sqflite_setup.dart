23cc984357a5902d3ad44d37e6eef8ad648456fb
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2023-10-07 00:22:43 +0530

    Display welcome note #27 : add welcome note insertion query in sqflit… (#64)
    * Display welcome note #27 : add welcome note insertion query in sqflite_setup.dart

--- lib/core/databases/sqflite_setup.dart
+++ lib/core/databases/sqflite_setup.dart
@@ -60,8 +60,28 @@ class DBProvider {
               ${NoteDependencies.ASSET_PATH} TEXT
             )
             """);
+          log.i("Inserting welcome note");
+          //as soon as the tables are created, insert a welcome note
+
+          String body = """[{\"insert\":\"Welcome to DiaryVault!\\n\\nKey Features:\\n\\n-Rich text editor with support for images and videos.\\n\\n-Your data is securely preserved on your Google Drive / Dropbox account, ensuring complete ownership and privacy\\r.\\n\\n-Sync data between multiple devices.\\r\\n\\n-Fingerprint login on supported devices.\\r\\n\\n-Multiple Themes.\\n\\n\\nHappy notemaking!\\nThe DiaryVault team.\\n\\n\"}]""";
+
+          Map<String,Object> notemap = {
+            Notes.ID: "f773a170-6447-11ee-9a76-c314b6be99a3",
+            Notes.CREATED_AT: DateTime.now().millisecondsSinceEpoch,
+            Notes.TITLE: "Welcome to DiaryVault",
+            Notes.BODY: body,
+            Notes.HASH: "welcome_note_hash",
+            Notes.LAST_MODIFIED: DateTime.now().millisecondsSinceEpoch,
+            Notes.PLAIN_TEXT: "Read me to get started!",
+            Notes.DELETED: 0,
+            Notes.AUTHOR_ID: "guest_user_id",
+          };
+          await db.insert(Notes.TABLE_NAME, notemap);
 
           log.i("All create queries executed successfully");
+          log.i("Welcome Note inserted into table: ${Notes.TABLE_NAME}");
+        //   print note
+          await db.query(Notes.TABLE_NAME).then((value) => log.i(value));
         } catch (e) {
           log.e(e);
           rethrow;
