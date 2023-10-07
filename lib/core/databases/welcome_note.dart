89ae41296d857fe055d20cd9955023583118d2d1
Sanketh B K <sanketh.bk@razorpay.com>
2023-10-07 20:38:19 +0530

    Added welcome note details in a class

---
+++ lib/core/databases/welcome_note.dart
@@ -0,0 +1,13 @@
+class WelcomeNote {
+  static String ID = "f773a170-6447-11ee-9a76-c314b6be99a3";
+  static int CREATED_AT = 1696597075215;
+  static String TITLE = "Welcome to DiaryVault";
+  static String BODY =
+      '[{\"insert\":\"This\",\"attributes\":{\"bold\":true}},{\"insert\":\"is\",\"attributes\":{\"bold\":true,\"italic\":true}},{\"insert\":\"welcome\"},{\"insert\":\"note\",\"attributes\":{\"color\":\"#FF03A9F4\"}},{\"insert\":\"\\n\"}]';
+  static String HASH = "welcome_note_hash";
+  static int LAST_MODIFIED = 1696597122553;
+  static String PLAIN_TEXT =
+      "This is a welcome note. You can delete this note and start writing your own notes.";
+  static int DELETED = 0;
+  static String AUTHOR_ID = "guest_user_id";
+}
