6be55c107cb96bddf51883eff3e24a26a6b981b2
Vaibhav P <113239561+vaibhavppandey@users.noreply.github.com>
2023-10-07 15:46:48 +0530

    Refactored &  fixed NoteRead feature  (#65)
    * Updated the file by adding pause button functionality and created read_notes.dart in widgets directory. (#62)
    
    ---------
    
    Co-authored-by: Pratibhanu Jarngal <99665583+Night-Amber3301@users.noreply.github.com>

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,7 @@
 #include <file_selector_windows/file_selector_windows.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <flutter_tts/flutter_tts_plugin.h>
 #include <local_auth_windows/local_auth_plugin.h>
 #include <pasteboard/pasteboard_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
@@ -23,6 +24,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  FlutterTtsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
   LocalAuthPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("LocalAuthPlugin"));
   PasteboardPluginRegisterWithRegistrar(
