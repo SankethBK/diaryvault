a8203ebac7544441e7e570a07e9da7201713f355
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2023-10-13 19:27:21 +0530

    #57: add guest login test with failing note creation.

--- integration_test/test_guest_login.dart
+++ integration_test/test_guest_login.dart
@@ -1,21 +1,56 @@
 import 'package:dairy_app/app/view/app.dart';
 import 'package:dairy_app/core/pages/home_page.dart';
-import 'package:dairy_app/features/auth/presentation/pages/auth_page.dart';
+import 'package:dairy_app/core/widgets/glassmorphism_cover.dart';
+import 'package:dairy_app/features/notes/presentation/pages/note_create_page.dart';
+import 'package:dairy_app/features/notes/presentation/widgets/note_save_button.dart';
+import 'package:dairy_app/features/notes/presentation/widgets/note_title_input_field.dart';
+import 'package:dairy_app/features/notes/presentation/widgets/notes_close_button.dart';
+import 'package:dairy_app/features/notes/presentation/widgets/rich_text_editor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:dairy_app/core/dependency_injection/injection_container.dart'
+as di;
 
-Future<void> main() async{
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('Guest login test, click on sign up button to move reveal guest login button'
-      'click the guest login button to enter HomePage', (WidgetTester tester) async {
-    await tester.pumpWidget(App());
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Sign up'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Continue as guest'));
-    await tester.pumpAndSettle();
-    expect(find.byType(HomePage), findsOneWidget);
-    expect(find.byType(AuthPage), findsNothing);
+Future<void> main() async {
+  setUpAll(() async {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load();
+    await di.init();
   });
+
+  testWidgets(
+      'Guest login integration test,'
+          'click on sign up button to move reveal guest login button'
+          'click the guest login button to enter HomePage'
+          'Click on the floating action button to move to NoteCreatePage'
+          'Enter some text in the title field'
+          'Enter some text in the body field'
+          'Click on the save button to save the note'
+          'Click on the back button to move back to HomePage'
+          'Check if the note is saved in the HomePage',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(const App());
+        await tester.pumpAndSettle();
+        await tester.tap(find.text('Continue as guest'));
+        await tester.pumpAndSettle();
+        expect(find.byType(HomePage), findsOneWidget);
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(FloatingActionButton));
+        await tester.pumpAndSettle();
+        // input some text in title field
+        await tester.enterText(find.byType(NoteTitleInputField), 'Test Title');
+        await tester.pumpAndSettle();
+        await tester.enterText(find.byType(QuillEditor), 'Test Body');
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(NoteSaveButton));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(NotesCloseButton));
+        await tester.pumpAndSettle();
+        expect(find.text('Test Title'), findsOneWidget);
+        expect(find.byType(HomePage), findsOneWidget);
+        expect(find.byType(NoteCreatePage), findsNothing);
+          });
 }
