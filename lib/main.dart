67cd86a147cd393a8dbffafef00f82f04f09507c
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2023-10-22 01:02:08 +0530

    Retrieve dropbox token from proxy (#108)
    Retrieve dropbox token from proxy

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:dairy_app/app/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'app/view/app.dart';
@@ -13,7 +12,6 @@ Future<void> main() async {
   Bloc.observer = AppBlocObserver();
 
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
 
   SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
