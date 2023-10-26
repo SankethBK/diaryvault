9913a2d6ce29cbf71687ad1558cdb5730606e787
Nenza Nurfirmansyah <nenza.dev@gmail.com>
2023-10-26 10:20:42 +0700

    add id-Basaha language

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sanketh.bk/sdk/flutter3"
-export "FLUTTER_APPLICATION_PATH=/Users/sanketh.bk/my_projects/flutter/apps in production/dairy_app"
+export "FLUTTER_ROOT=/Users/user/Documents/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user/Documents/Projects/Solo Project/diaryvault"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.1.1"
-export "FLUTTER_BUILD_NUMBER=7"
+export "FLUTTER_BUILD_NAME=2.1.4"
+export "FLUTTER_BUILD_NUMBER=10"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
