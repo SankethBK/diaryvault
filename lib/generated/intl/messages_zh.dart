d41817abad20c6325dcb4629595d38a24238e14f
Sanketh B K <sanketh.bk@razorpay.com>
2023-11-17 21:23:39 +0530

    move es translation to correct file

--- lib/generated/intl/messages_zh.dart
+++ lib/generated/intl/messages_zh.dart
@@ -20,7 +20,7 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'zh';
 
-  static String m0(time) => "将在 ${time}收到提醒";
+  static String m0(time) => "将在 ${time} 收到提醒";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
@@ -28,7 +28,7 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("账户创建成功"),
         "alreadyHaveAnAccount": MessageLookupByLibrary.simpleMessage("已有帐户？"),
         "appDescription": MessageLookupByLibrary.simpleMessage(
-            "遇见 diaryVault - 一款帮您轻松记录想法与回忆的日记应用。已上线Play Store！"),
+            "遇见 diaryVault - 一款帮您轻松记录想法、回忆和精彩瞬间的日记应用。已上线Play Store！"),
         "appLanguage": MessageLookupByLibrary.simpleMessage("语言"),
         "appVersion": MessageLookupByLibrary.simpleMessage("应用版本"),
         "areYouSureAboutLoggingOut":
@@ -41,13 +41,13 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("支持的同步平台"),
         "byContinuingYouAgree":
             MessageLookupByLibrary.simpleMessage("继续表示您同意我们的"),
-        "camera": MessageLookupByLibrary.simpleMessage("相机"),
+        "camera": MessageLookupByLibrary.simpleMessage("拍照"),
         "cancel": MessageLookupByLibrary.simpleMessage("取消"),
         "changeEmail": MessageLookupByLibrary.simpleMessage("修改邮箱"),
         "changePassword": MessageLookupByLibrary.simpleMessage("修改密码"),
         "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage("选择同步源"),
         "chooseTheme": MessageLookupByLibrary.simpleMessage("选择主题"),
-        "chooseTime": MessageLookupByLibrary.simpleMessage("选择主题"),
+        "chooseTime": MessageLookupByLibrary.simpleMessage("选择时间"),
         "closeTheApp": MessageLookupByLibrary.simpleMessage("关闭应用？"),
         "confirmNewPassword": MessageLookupByLibrary.simpleMessage("确认新密码"),
         "continueAsGues": MessageLookupByLibrary.simpleMessage("试用"),
@@ -66,21 +66,21 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("开启指纹登录"),
         "enterCurrentPassword": MessageLookupByLibrary.simpleMessage("输入密码"),
         "enterNewEmail": MessageLookupByLibrary.simpleMessage("输入新邮箱"),
-        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage("输入已注册邮箱"),
+        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage("输入邮箱"),
         "exportNotes": MessageLookupByLibrary.simpleMessage("导出笔记"),
         "exportToPDF": MessageLookupByLibrary.simpleMessage("导出为PDF （测试功能）"),
         "exportToPlainText": MessageLookupByLibrary.simpleMessage("导出为文本"),
         "failedToFetchNote": MessageLookupByLibrary.simpleMessage("获取笔记失败"),
         "failedToSaveNote": MessageLookupByLibrary.simpleMessage("保存笔记失败"),
         "fingerPrintAthShouldBeEnabledInDeviceSettings":
-            MessageLookupByLibrary.simpleMessage("在系统设置中开启指纹登录"),
+            MessageLookupByLibrary.simpleMessage("请先在系统设置中开启指纹解锁"),
         "fingerprintLoginFailed":
             MessageLookupByLibrary.simpleMessage("指纹登陆失败"),
         "forgotPassword": MessageLookupByLibrary.simpleMessage("忘记密码"),
         "from": MessageLookupByLibrary.simpleMessage("从"),
         "gallery": MessageLookupByLibrary.simpleMessage("相册"),
         "getDailyReminders":
-            MessageLookupByLibrary.simpleMessage("在选定的时间发送通知以保持更新"),
+            MessageLookupByLibrary.simpleMessage("在选定的时间通知您记日记"),
         "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
         "incorrectPassword": MessageLookupByLibrary.simpleMessage("密码错误"),
         "language": MessageLookupByLibrary.simpleMessage("中文"),
@@ -94,6 +94,7 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("请登录以开启自动同步"),
         "moreInfo": MessageLookupByLibrary.simpleMessage("更多信息"),
         "newPassword": MessageLookupByLibrary.simpleMessage("新密码"),
+        "nextCloud": MessageLookupByLibrary.simpleMessage("NextCloud"),
         "notAvailable": MessageLookupByLibrary.simpleMessage("未知"),
         "noteSavedSuccessfully": MessageLookupByLibrary.simpleMessage("笔记保存成功"),
         "noteUpdatedSuccessfully":
@@ -105,7 +106,7 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("未选择提醒时间"),
         "notificationTitle1": MessageLookupByLibrary.simpleMessage("日记时间！"),
         "notificationsNotEnabled":
-            MessageLookupByLibrary.simpleMessage("未开启通知"),
+            MessageLookupByLibrary.simpleMessage("未开启提醒"),
         "pageNotFound": MessageLookupByLibrary.simpleMessage("未找到页面"),
         "passwordResetMailSent":
             MessageLookupByLibrary.simpleMessage("重置密码邮件已发送"),
@@ -137,7 +138,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "tooManyWrongAttempts":
             MessageLookupByLibrary.simpleMessage("尝试次数过多，请使用密码登录"),
         "unexpectedErrorOccured": MessageLookupByLibrary.simpleMessage("未知错误"),
-        "video": MessageLookupByLibrary.simpleMessage("视频"),
+        "video": MessageLookupByLibrary.simpleMessage("录像"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("WebDAV URL"),
         "youHaveUnsavedChanges": MessageLookupByLibrary.simpleMessage("修改未保存"),
         "youWillBeNotifiedAt": m0
       };
