e2fcbe0aef36e375d0624a1b61994bcf52bd96d0
Mohammad Anann <mohammadannan@dyn-172-20-98-2.qatar.cmu.local>
2023-12-03 17:08:38 +0300

    Resolving lint errrors for pin_auth_repository.dart, which was also renamed to avoid camelCase

--- lib/features/auth/data/repositories/pin_auth_repository.dart
+++ lib/features/auth/data/repositories/pin_auth_repository.dart
@@ -3,14 +3,11 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:crypto/crypto.dart';
 
 class PINAuthRepository {
-  final storage = new FlutterSecureStorage();
-
+  // ignore: prefer_const_constructors
+  final storage = FlutterSecureStorage();
   Future<void> savePIN(String userId, String pin) async {
-    // Hash the pin before storing it for security reasons
     var bytes = utf8.encode(pin); // data being hashed
     var hashedPIN = sha256.convert(bytes).toString();
-
-    // Store the hashed pin with the userId as part of the key
     await storage.write(key: '${userId}_PIN', value: hashedPIN);
   }
 
@@ -20,19 +17,13 @@ class PINAuthRepository {
   }
 
   Future<bool> verifyPIN(String userId, String pin) async {
-    // Hash the pin for verification
     var bytes = utf8.encode(pin); // data being hashed
     var hashedPIN = sha256.convert(bytes).toString();
-
-    // Retrieve the hashed pin using the userId
     String? storedHashedPIN = await storage.read(key: '${userId}_PIN');
-
-    // Return true if the hashed pin matches the stored hashed pin
     return storedHashedPIN == hashedPIN;
   }
 
   Future<void> deletePIN(String userId) async {
-    // Delete the pin for the user
     await storage.delete(key: '${userId}_PIN');
   }
 }
