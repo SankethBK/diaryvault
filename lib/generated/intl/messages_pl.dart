5bfbe92c2a0ee022302be5430cd918b851f24778
Kacper Lukasz Nowicki <92367539+nokacper24@users.noreply.github.com>
2023-12-06 22:08:59 +0100

    Feat/polish translations (#170)
    * add Polish translations
    
    * regenerate localization files with Flutter Intl plugin
    
    necessary after adding Polish translations.
    files for Telugu were not generated before, including them here

---
+++ lib/generated/intl/messages_pl.dart
@@ -0,0 +1,189 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a pl locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'pl';
+
+  static String m0(time) => "Zostaniesz powiadomiony/a o ${time}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "accountSetupSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Konfiguracja konta ukończona sukcesem"),
+        "alreadyHaveAnAccount":
+            MessageLookupByLibrary.simpleMessage("Masz już konto?"),
+        "appDescription": MessageLookupByLibrary.simpleMessage(
+            "DiaryVault - aplikacja pamiętnik, stworzona w celu łatwego zapisywania myśli, wspomnień i chwil. Dostępna w Play Store!"),
+        "appLanguage": MessageLookupByLibrary.simpleMessage("Język aplikacji"),
+        "appVersion": MessageLookupByLibrary.simpleMessage("Wersja aplikacji"),
+        "areYouSureAboutLoggingOut": MessageLookupByLibrary.simpleMessage(
+            "Na pewno chcesz się wylogować?"),
+        "autoSync": MessageLookupByLibrary.simpleMessage("Auto-synchronizacja"),
+        "automaticallySave": MessageLookupByLibrary.simpleMessage(
+            "Zapisz notatki automatycznie co 10 sekund"),
+        "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
+            "Synchronizuj notatki z chmurą automatycznie"),
+        "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
+            "Dostępne platformy do synchronizacji"),
+        "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
+            "Kontynuując, akceptujesz naszą"),
+        "camera": MessageLookupByLibrary.simpleMessage("Aparat"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Anuluj"),
+        "changeEmail": MessageLookupByLibrary.simpleMessage("Zmień email"),
+        "changePassword": MessageLookupByLibrary.simpleMessage("Zmień hasło"),
+        "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
+            "Wybierz sposób synchronizacji"),
+        "chooseTheme": MessageLookupByLibrary.simpleMessage("Wybierz motyw"),
+        "chooseTime": MessageLookupByLibrary.simpleMessage("Wybierz godzinę"),
+        "closeTheApp":
+            MessageLookupByLibrary.simpleMessage("Wyłączyć aplikację?"),
+        "confirmNewPassword":
+            MessageLookupByLibrary.simpleMessage("Potwierdź nowe hasło"),
+        "continueAsGues":
+            MessageLookupByLibrary.simpleMessage("Kontynuuj jako gość"),
+        "dailyReminders":
+            MessageLookupByLibrary.simpleMessage("Codzienne przypomnienia"),
+        "dateFilter":
+            MessageLookupByLibrary.simpleMessage("Filtruj według daty"),
+        "delete": MessageLookupByLibrary.simpleMessage("Usuń"),
+        "deletionFailed":
+            MessageLookupByLibrary.simpleMessage("Usunięcie nie powiodło się"),
+        "done": MessageLookupByLibrary.simpleMessage("Gotowe"),
+        "dontHaveAccount":
+            MessageLookupByLibrary.simpleMessage("Nie masz konta?"),
+        "dropbox": MessageLookupByLibrary.simpleMessage("Dropbox"),
+        "emailUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "Email zaktualizowany pomyślnie, zaloguj się na nowo"),
+        "enableAutoSave":
+            MessageLookupByLibrary.simpleMessage("Włącz autozapis"),
+        "enableDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "Włącz codzienne przypomnienia"),
+        "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
+            "Logowanie czytnikiem linii papilarnych"),
+        "enterCurrentPassword":
+            MessageLookupByLibrary.simpleMessage("Wprowadź aktualne hasło"),
+        "enterNewEmail":
+            MessageLookupByLibrary.simpleMessage("Wprowadź nowy email"),
+        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage(
+            "Wprowadź zarejestrowany email"),
+        "exportNotes":
+            MessageLookupByLibrary.simpleMessage("Eksportuj notatki"),
+        "exportToPDF":
+            MessageLookupByLibrary.simpleMessage("Eksportuj do PDF (beta)"),
+        "exportToPlainText":
+            MessageLookupByLibrary.simpleMessage("Eksportuj jako tekst"),
+        "failedToFetchNote": MessageLookupByLibrary.simpleMessage(
+            "Nie udało się pobrać notatki"),
+        "failedToSaveNote": MessageLookupByLibrary.simpleMessage(
+            "Nie udało się zapisać notatki"),
+        "fingerPrintAthShouldBeEnabledInDeviceSettings":
+            MessageLookupByLibrary.simpleMessage(
+                "Logowanie czytnikiem linii papilarnych musi zostać włączone w ustawieniach urządzenia"),
+        "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
+            "Logowanie czytnikiem papilarnym nie powiodło się"),
+        "fontFamily": MessageLookupByLibrary.simpleMessage("Czcionka"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Nie pamiętasz hasła?"),
+        "from": MessageLookupByLibrary.simpleMessage("Od"),
+        "gallery": MessageLookupByLibrary.simpleMessage("Galeria"),
+        "getDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "Otrzymuj codzienne przypomnienia o wybranej porze, aby aktualizować pamiętnik."),
+        "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
+        "incorrectPassword":
+            MessageLookupByLibrary.simpleMessage("Nieprawidłowe hasło"),
+        "language": MessageLookupByLibrary.simpleMessage("Polski"),
+        "lastSynced":
+            MessageLookupByLibrary.simpleMessage("Ostatnia synchronizacja: "),
+        "leave": MessageLookupByLibrary.simpleMessage("Wyjdź"),
+        "link": MessageLookupByLibrary.simpleMessage("Link"),
+        "logIn": MessageLookupByLibrary.simpleMessage("Zaloguj się"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Wyloguj się"),
+        "logOut2": MessageLookupByLibrary.simpleMessage("Wyloguj się"),
+        "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
+            "Zaloguj się aby włączyć auto-synchronizację"),
+        "moreInfo": MessageLookupByLibrary.simpleMessage("Więcej informacji"),
+        "newPassword": MessageLookupByLibrary.simpleMessage("Nowe hasło"),
+        "nextCloud": MessageLookupByLibrary.simpleMessage("NextCloud"),
+        "notAvailable": MessageLookupByLibrary.simpleMessage("Niedostępne"),
+        "noteSavedSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Notatka zapisana pomyślnie"),
+        "noteUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "Notatka zaktualizowana pomyślnie"),
+        "notesSyncSuccessfull":
+            MessageLookupByLibrary.simpleMessage("Synchronizacja pomyślna"),
+        "notificationDescription1": MessageLookupByLibrary.simpleMessage(
+            "Poświęć kilka minut na refleksję i wpis w pamiętniku"),
+        "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "Nie wybrano godziny powiadomień"),
+        "notificationTitle1":
+            MessageLookupByLibrary.simpleMessage("Czas na wpisy w pamiętniku!"),
+        "notificationsNotEnabled":
+            MessageLookupByLibrary.simpleMessage("Powiadomienia wyłączone"),
+        "pageNotFound": MessageLookupByLibrary.simpleMessage(
+            "Strona nie została odnaleziona"),
+        "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
+            "wysłano email z linkiem do zresetowania hasła"),
+        "passwordResetSuccessful":
+            MessageLookupByLibrary.simpleMessage("Hasło zmienione pomyślnie"),
+        "passwordVerified":
+            MessageLookupByLibrary.simpleMessage("Hasło zweryfikowane"),
+        "passwordsDontMatch":
+            MessageLookupByLibrary.simpleMessage("Hasła nie są takie same"),
+        "pleaseSetupYourAccountToUseThisFeature":
+            MessageLookupByLibrary.simpleMessage(
+                "Zaloguj się, aby użyć tej funkcji"),
+        "privacyPolicy":
+            MessageLookupByLibrary.simpleMessage("Politykę Prywatności"),
+        "resetPassword": MessageLookupByLibrary.simpleMessage("Zmień hasło"),
+        "securitySettings":
+            MessageLookupByLibrary.simpleMessage("Ustawienia bezpieczeństwa"),
+        "sendFeedback": MessageLookupByLibrary.simpleMessage("Wyślij opinię"),
+        "settings": MessageLookupByLibrary.simpleMessage("Ustawienia"),
+        "setupYourAccount":
+            MessageLookupByLibrary.simpleMessage("Konfiguruj swoje konto"),
+        "shareWithFriends":
+            MessageLookupByLibrary.simpleMessage("Udostępnij znajomym"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Zaloguj się"),
+        "signUp": MessageLookupByLibrary.simpleMessage("Zarejestruj się"),
+        "signedInAs": MessageLookupByLibrary.simpleMessage("Zalogowany jako"),
+        "sortByAtoZ":
+            MessageLookupByLibrary.simpleMessage("Sortuj alfabetycznie"),
+        "sortByLatestFirst":
+            MessageLookupByLibrary.simpleMessage("Sortuj od najnowszych"),
+        "sortByOldestFirst":
+            MessageLookupByLibrary.simpleMessage("Sortuj od najstarszych"),
+        "stay": MessageLookupByLibrary.simpleMessage("Zostań"),
+        "submit": MessageLookupByLibrary.simpleMessage("Zatwierdź"),
+        "syncNow": MessageLookupByLibrary.simpleMessage("Synchronizuj teraz"),
+        "tagAlreadyExists":
+            MessageLookupByLibrary.simpleMessage("Etykieta już istnieje"),
+        "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
+            "Kliknij tutaj, aby rozwinąć tytuł"),
+        "to": MessageLookupByLibrary.simpleMessage("Do"),
+        "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
+            "Za dużo nieudanych prób, zaloguj się hasłem"),
+        "unexpectedErrorOccured":
+            MessageLookupByLibrary.simpleMessage("Niespodziewany błąd"),
+        "video": MessageLookupByLibrary.simpleMessage("Kamera"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("WebDAV URL"),
+        "youHaveUnsavedChanges":
+            MessageLookupByLibrary.simpleMessage("Masz niezapisane zmiany"),
+        "youWillBeNotifiedAt": m0
+      };
+}
---
+++ lib/generated/intl/messages_te.dart
@@ -0,0 +1,192 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a te locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'te';
+
+  static String m0(time) => "మీకు ${time}కి తెలియజేయబడుతుంది";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "accountSetupSuccessful":
+            MessageLookupByLibrary.simpleMessage("ఖాతా సెటప్ విజయవంతమైంది"),
+        "alreadyHaveAnAccount":
+            MessageLookupByLibrary.simpleMessage("ఖాతా కలిగి ఉన్నారా?"),
+        "appDescription": MessageLookupByLibrary.simpleMessage(
+            "డిస్కవర్ డైరీవాల్ట్ - మీ ఆలోచనలు, జ్ఞాపకాలు మరియు క్షణాలను అప్రయత్నంగా క్యాప్చర్ చేయడంలో మీకు సహాయపడటానికి రూపొందించబడిన డైరీ యాప్. ప్లే స్టోర్‌లో ఇప్పుడు అందుబాటులో ఉంది!"),
+        "appLanguage": MessageLookupByLibrary.simpleMessage("యాప్ భాష"),
+        "appVersion": MessageLookupByLibrary.simpleMessage("యాప్ వెర్షన్"),
+        "areYouSureAboutLoggingOut": MessageLookupByLibrary.simpleMessage(
+            "మీరు ఖచ్చితంగా లాగ్ అవుట్ చేయాలనుకుంటున్నారా?"),
+        "autoSync": MessageLookupByLibrary.simpleMessage("స్వీయ సమకాలీకరణ"),
+        "automaticallySave": MessageLookupByLibrary.simpleMessage(
+            "ప్రతి 10 సెకన్ల తర్వాత మీ గమనికలను స్వయంచాలకంగా సేవ్ చేస్తుంది"),
+        "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
+            "స్వయంచాలకంగా క్లబ్ తో నోట్స్"),
+        "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
+            "సమకాలీకరణ కోసం అందుబాటులో ఉన్న ప్లాట్‌ఫారమ్‌లు"),
+        "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
+            "కొనసాగించడం ద్వారా మీరు అంగీకరిస్తున్నారు"),
+        "camera": MessageLookupByLibrary.simpleMessage("కెమెరా"),
+        "cancel": MessageLookupByLibrary.simpleMessage("రద్దు చేయండి"),
+        "changeEmail":
+            MessageLookupByLibrary.simpleMessage("ఈ - మెయిల్ ను మార్చండి"),
+        "changePassword":
+            MessageLookupByLibrary.simpleMessage("పాస్వర్డ్ మార్చండి"),
+        "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
+            "సమకాలీకరణ మూలాన్ని ఎంచుకోండి"),
+        "chooseTheme":
+            MessageLookupByLibrary.simpleMessage("థీమ్‌ని ఎంచుకోండి"),
+        "chooseTime":
+            MessageLookupByLibrary.simpleMessage("సమయాన్ని ఎంచుకోండి"),
+        "closeTheApp":
+            MessageLookupByLibrary.simpleMessage("యాప్‌ను మూసివేయాలా?"),
+        "confirmNewPassword": MessageLookupByLibrary.simpleMessage(
+            "కొత్త పాస్‌వర్డ్‌ను నిర్ధారించండి"),
+        "continueAsGues":
+            MessageLookupByLibrary.simpleMessage("అతిథిగా కొనసాగండి"),
+        "dailyReminders":
+            MessageLookupByLibrary.simpleMessage("రోజువారీ రిమైండర్‌లు"),
+        "dateFilter": MessageLookupByLibrary.simpleMessage("తేదీ ఫిల్టర్"),
+        "delete": MessageLookupByLibrary.simpleMessage("తొలగించు"),
+        "deletionFailed":
+            MessageLookupByLibrary.simpleMessage("తొలగింపు విఫలమైంది"),
+        "done": MessageLookupByLibrary.simpleMessage("పూర్తయింది"),
+        "dontHaveAccount": MessageLookupByLibrary.simpleMessage("ఖాతా లేదా?"),
+        "dropbox": MessageLookupByLibrary.simpleMessage("డ్రాప్‌బాక్స్"),
+        "emailUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "ఇమెయిల్ విజయవంతంగా నవీకరించబడింది, దయచేసి మళ్ళీ లాగిన్ చేయండి"),
+        "enableAutoSave": MessageLookupByLibrary.simpleMessage(
+            "స్వయంచాలకంగా సేవ్ చేయడాన్ని ప్రారంభించండి"),
+        "enableDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "రోజువారీ రిమైండర్‌లను ప్రారంభించండి"),
+        "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
+            "ఫింగర్‌ప్రింట్‌లాగిన్‌ని ప్రారంభించండి"),
+        "enterCurrentPassword": MessageLookupByLibrary.simpleMessage(
+            "ప్రస్తుత పాస్‌వర్డ్‌న్ని ప్రవేశపెట్టండి"),
+        "enterNewEmail":
+            MessageLookupByLibrary.simpleMessage("కొత్త ఈమెయిలును ఇవ్వండి"),
+        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage(
+            "నమోదు చేసిన ఈమెయిలును ఇవ్వండి"),
+        "exportNotes":
+            MessageLookupByLibrary.simpleMessage("మీ గమనికలను ఎగుమతి చేయండి"),
+        "exportToPDF":
+            MessageLookupByLibrary.simpleMessage("PDF (బీటా)కి ఎగుమతి చేయండి"),
+        "exportToPlainText":
+            MessageLookupByLibrary.simpleMessage("సాదా వచనానికి ఎగుమతి చేయండి"),
+        "failedToFetchNote":
+            MessageLookupByLibrary.simpleMessage("ToFetchNote విఫలమైంది"),
+        "failedToSaveNote": MessageLookupByLibrary.simpleMessage(
+            "గమనికను సేవ్ చేయడంలో విఫలమైంది"),
+        "fingerPrintAthShouldBeEnabledInDeviceSettings":
+            MessageLookupByLibrary.simpleMessage(
+                "పరికర సెట్టింగ్‌లలో వేలిముద్ర ప్రమాణీకరణను ప్రారంభించాలి"),
+        "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
+            "ఫింగర్ ప్రింట్ లాగిన్ విఫలమైంది"),
+        "fontFamily": MessageLookupByLibrary.simpleMessage("ఫాంట్ కుటుంబం"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("పాస్‌వర్డ్ మర్చిపోయారా"),
+        "from": MessageLookupByLibrary.simpleMessage("నుండి"),
+        "gallery": MessageLookupByLibrary.simpleMessage("గ్యాలరీ"),
+        "getDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "మీ జర్నల్‌ను తాజాగా ఉంచడానికి మీరు ఎంచుకున్న సమయంలో రోజువారీ రిమైండర్‌లను పొందండి."),
+        "googleDrive": MessageLookupByLibrary.simpleMessage("గూగుల్ డ్రైవ్"),
+        "incorrectPassword":
+            MessageLookupByLibrary.simpleMessage("సరికాని పాస్వర్డ్"),
+        "language": MessageLookupByLibrary.simpleMessage("Telugu"),
+        "lastSynced":
+            MessageLookupByLibrary.simpleMessage("చివరిగా సమకాలీకరించబడింది:"),
+        "leave": MessageLookupByLibrary.simpleMessage("వదిలేయండి"),
+        "link": MessageLookupByLibrary.simpleMessage("లింక్"),
+        "logIn": MessageLookupByLibrary.simpleMessage("లాగిన్"),
+        "logOut": MessageLookupByLibrary.simpleMessage("లాగ్ అవుట్ చేయండి"),
+        "logOut2": MessageLookupByLibrary.simpleMessage("లాగ్అవుట్"),
+        "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
+            "దయచేసి స్వీయ-సమకాలీకరణను ప్రారంభించడానికి లాగిన్ చేయండి"),
+        "moreInfo": MessageLookupByLibrary.simpleMessage("మరింత సమాచారం"),
+        "newPassword": MessageLookupByLibrary.simpleMessage("కొత్త పాస్‌వర్డ్"),
+        "nextCloud": MessageLookupByLibrary.simpleMessage("నెక్స్ట్ క్లౌడ్"),
+        "notAvailable": MessageLookupByLibrary.simpleMessage("అందుబాటులో లేదు"),
+        "noteSavedSuccessfully":
+            MessageLookupByLibrary.simpleMessage("గమనిక విజయవంతంగా భద్రమైంది"),
+        "noteUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "గమనిక విజయవంతంగా నవీకరించబడింది"),
+        "notesSyncSuccessfull": MessageLookupByLibrary.simpleMessage(
+            "గమనికల సమకాలీకరణ విజయవంతమైంది"),
+        "notificationDescription1": MessageLookupByLibrary.simpleMessage(
+            "మీ డైరీలో మీ రోజును ప్రతిబింబించడానికి కొన్ని నిమిషాలు కేటాయించండి"),
+        "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "మీరు నోటిఫికేషన్ సమయాన్ని ఎంచుకోలేదు"),
+        "notificationTitle1":
+            MessageLookupByLibrary.simpleMessage("జర్నల్‌కి సమయం!"),
+        "notificationsNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "నోటిఫికేషన్‌లు ప్రారంభించబడలేదు"),
+        "pageNotFound": MessageLookupByLibrary.simpleMessage("పేజీ కనబడలేదు"),
+        "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
+            "పాస్‌వర్డ్ రీసెట్ ఇమెయిల్ పంపబడింది"),
+        "passwordResetSuccessful": MessageLookupByLibrary.simpleMessage(
+            "పాస్‌వర్డ్ రీసెట్ విజయవంతమైంది"),
+        "passwordVerified":
+            MessageLookupByLibrary.simpleMessage("పాస్‌వర్డ్ ధృవీకరించబడింది"),
+        "passwordsDontMatch":
+            MessageLookupByLibrary.simpleMessage("పాస్‌వర్డ్‌లు సరిపోలడం లేదు"),
+        "pleaseSetupYourAccountToUseThisFeature":
+            MessageLookupByLibrary.simpleMessage(
+                "దయచేసి ఈ ఫీచర్‌ని ఉపయోగించడానికి మీ ఖాతాను సెటప్ చేయండి"),
+        "privacyPolicy": MessageLookupByLibrary.simpleMessage("గోప్యతా విధానం"),
+        "resetPassword":
+            MessageLookupByLibrary.simpleMessage("పాస్‌వర్డ్‌న్ని మార్చుకోండి"),
+        "securitySettings":
+            MessageLookupByLibrary.simpleMessage("భద్రతా సెట్టింగులు"),
+        "sendFeedback":
+            MessageLookupByLibrary.simpleMessage("అభిప్రాయాన్ని పంపండి"),
+        "settings": MessageLookupByLibrary.simpleMessage("సెట్టింగులు"),
+        "setupYourAccount":
+            MessageLookupByLibrary.simpleMessage("మీ ఖాతాను సెటప్ చేయండి"),
+        "shareWithFriends":
+            MessageLookupByLibrary.simpleMessage("మిత్రులతో పంచుకొనుట"),
+        "signIn": MessageLookupByLibrary.simpleMessage("సైన్ ఇన్ చేయండి"),
+        "signUp": MessageLookupByLibrary.simpleMessage("సైన్ అప్ చేయండి"),
+        "signedInAs":
+            MessageLookupByLibrary.simpleMessage("గా సైన్ ఇన్ చేసారు"),
+        "sortByAtoZ":
+            MessageLookupByLibrary.simpleMessage("A-Z ద్వారా క్రమబద్ధీకరించు"),
+        "sortByLatestFirst": MessageLookupByLibrary.simpleMessage(
+            "లేటెస్ట్ ఫస్ట్ వారీగా క్రమబద్ధీకరించండి"),
+        "sortByOldestFirst": MessageLookupByLibrary.simpleMessage(
+            "పాతది మొదటగా క్రమబద్ధీకరించండి"),
+        "stay": MessageLookupByLibrary.simpleMessage("ఉండు"),
+        "submit": MessageLookupByLibrary.simpleMessage("సమర్పించండి"),
+        "syncNow": MessageLookupByLibrary.simpleMessage("సమకాలీకరించు"),
+        "tagAlreadyExists":
+            MessageLookupByLibrary.simpleMessage("ట్యాగ్ ఇప్పటికే ఉంది"),
+        "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
+            "శీర్షికను విస్తరించడానికి ఇక్కడ నొక్కండి"),
+        "to": MessageLookupByLibrary.simpleMessage("కు"),
+        "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
+            "చాలా తప్పు ప్రయత్నాలు ఉన్నాయి, దయచేసి పాస్‌వర్డ్‌తో లాగిన్ చేయండి"),
+        "unexpectedErrorOccured":
+            MessageLookupByLibrary.simpleMessage("ఊహించని లోపం సంభవించింది"),
+        "video": MessageLookupByLibrary.simpleMessage("వీడియో"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("వెబ్DAV URL"),
+        "youHaveUnsavedChanges": MessageLookupByLibrary.simpleMessage(
+            "మీరు సేవ్ చేయని మార్పులను కలిగి ఉన్నారు"),
+        "youWillBeNotifiedAt": m0
+      };
+}
