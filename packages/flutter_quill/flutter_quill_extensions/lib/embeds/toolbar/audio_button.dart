12ad02e8a06fe2b712f018e7eb2da98e77992717
Sanketh B K <sanketh.bk@razorpay.com>
2023-12-17 02:01:59 +0530

    added logic to pick audio files

--- packages/flutter_quill/flutter_quill_extensions/lib/embeds/toolbar/audio_button.dart
+++ packages/flutter_quill/flutter_quill_extensions/lib/embeds/toolbar/audio_button.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
@@ -42,11 +43,19 @@ class AudioButton extends StatelessWidget {
   }
 
   Future<void> _onPressedHandler(BuildContext context) async {
-    print("on pressed handler called");
+    print('on pressed handler called');
 
     if (audioPickSetting != null) {
       final result = await audioPickSetting!(context);
-      print("user selected $result");
+      print('user selected $result');
+
+      if (result == AudioPickSetting.File) {
+        final path = await FilePicker.platform.pickFiles(
+          type: FileType.audio,
+        );
+
+        print('path = $path');
+      } else if (result == AudioPickSetting.Record) {}
     }
   }
 }
