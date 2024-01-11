bbdc37e778558e04a3fc8ac96f4ad9fcc3ddb3da
Marek Jakub <mj3935538@gmail.com>
2024-01-11 20:13:58 +0100

    #75 slovak translation feature (#194)
    * Add translations for Slovak.
    
    * Update message translations.
    
    * Add slovak translation.

---
+++ lib/generated/intl/messages_sk.dart
@@ -0,0 +1,215 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a sk locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'sk';
+
+  static String m0(time) => "Notifikácia o ${time}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "accountSetupSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Konfigurácia účtu bola úspešná"),
+        "alreadyHaveAnAccount":
+            MessageLookupByLibrary.simpleMessage("Máte už účet?"),
+        "appDescription": MessageLookupByLibrary.simpleMessage(
+            "Objavte diaryVault - aplikáciu s denníkom, ktorá vám pomôže bez námahy zachytiť vaše myšlienky, spomienky a momenty. Teraz k dispozícii v Obchode Play!"),
+        "appLanguage": MessageLookupByLibrary.simpleMessage("Jazyk aplikácie"),
+        "appVersion": MessageLookupByLibrary.simpleMessage("Verzia aplikácie"),
+        "areYouSureAboutLoggingOut":
+            MessageLookupByLibrary.simpleMessage("Chcete sa odhlásiť?"),
+        "autoSync": MessageLookupByLibrary.simpleMessage("Auto-synchronizácia"),
+        "automaticallySave": MessageLookupByLibrary.simpleMessage(
+            "Automaticky ukladá vaše poznámky každých 10 sekúnd"),
+        "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
+            "Synchronizovať obsah s cloudovým úložiskom automaticky"),
+        "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
+            "Dostupné platformy pre synchronizáciu"),
+        "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
+            "Pokračovaním súhlasíte s našimi"),
+        "camera": MessageLookupByLibrary.simpleMessage("Fotoaparát"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Zrušiť"),
+        "changeEmail": MessageLookupByLibrary.simpleMessage("Zmeniť email"),
+        "changePassword": MessageLookupByLibrary.simpleMessage("Zmeniť heslo"),
+        "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
+            "Vyberte spôsob synchronizácie"),
+        "chooseTheme": MessageLookupByLibrary.simpleMessage("Vybrať motív"),
+        "chooseTime": MessageLookupByLibrary.simpleMessage("Vybrať čas"),
+        "closeTheApp":
+            MessageLookupByLibrary.simpleMessage("Zavrieť aplikáciu?"),
+        "confirmNewPassword":
+            MessageLookupByLibrary.simpleMessage("Potvrdiť nové heslo"),
+        "confirmNewPin":
+            MessageLookupByLibrary.simpleMessage("Potvrďiť nový PIN kód"),
+        "continueAsGues":
+            MessageLookupByLibrary.simpleMessage("Prihlásenie ako hosť"),
+        "dailyReminders":
+            MessageLookupByLibrary.simpleMessage("Denné upozornenia"),
+        "dateFilter":
+            MessageLookupByLibrary.simpleMessage("Filtrujte podľa dátumu"),
+        "delete": MessageLookupByLibrary.simpleMessage("Vymazať"),
+        "deletionFailed":
+            MessageLookupByLibrary.simpleMessage("Zmazanie zlyhalo"),
+        "done": MessageLookupByLibrary.simpleMessage("Filtrovať"),
+        "dontHaveAccount": MessageLookupByLibrary.simpleMessage("Nemáte účet?"),
+        "dropbox": MessageLookupByLibrary.simpleMessage("Dropbox"),
+        "emailUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "Email bol úspešne aktualizovaný, prihláste sa znova"),
+        "enableAutoSave": MessageLookupByLibrary.simpleMessage(
+            "Povoliť automatické ukladanie"),
+        "enableDailyReminders":
+            MessageLookupByLibrary.simpleMessage("Povoliť denné upozornenia"),
+        "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
+            "Povoliť prihlásenie odtlačkom prsta"),
+        "enablePINLogin": MessageLookupByLibrary.simpleMessage(
+            "Povoliť prihlásenie pomocou kódu PIN"),
+        "enterCurrentPassword":
+            MessageLookupByLibrary.simpleMessage("Zadajte aktuálne heslo"),
+        "enterNewEmail":
+            MessageLookupByLibrary.simpleMessage("Zadajte nový email"),
+        "enterPin":
+            MessageLookupByLibrary.simpleMessage("Zadajte svoj PIN kód"),
+        "enterRegisteredEmail":
+            MessageLookupByLibrary.simpleMessage("Zadajte registrovaný email"),
+        "exportNotes":
+            MessageLookupByLibrary.simpleMessage("Exportovať poznámky"),
+        "exportToPDF":
+            MessageLookupByLibrary.simpleMessage("Exportovať do formátu PDF"),
+        "exportToPlainText":
+            MessageLookupByLibrary.simpleMessage("Exportovať do formátu Text"),
+        "failedToFetchNote": MessageLookupByLibrary.simpleMessage(
+            "Načítanie poznámky neúspešné"),
+        "failedToSaveNote":
+            MessageLookupByLibrary.simpleMessage("Uloženie poznámky neúspešné"),
+        "fingerPrintAthShouldBeEnabledInDeviceSettings":
+            MessageLookupByLibrary.simpleMessage(
+                "Autentifikácia odtlačkom prsta potrebuje povolenie v nastaveniach zariadenia"),
+        "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
+            "Prihlásenie odtlačkom prsta zlyhalo"),
+        "fontFamily": MessageLookupByLibrary.simpleMessage("Typ písma"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Zabudli ste heslo?"),
+        "from": MessageLookupByLibrary.simpleMessage("Od"),
+        "gallery": MessageLookupByLibrary.simpleMessage("Galéria"),
+        "getDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "Nastaviť denné upozornenia vo zvolenom čase, aby bol denník aktuálny."),
+        "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
+        "incorrectPassword":
+            MessageLookupByLibrary.simpleMessage("Nesprávne heslo"),
+        "language": MessageLookupByLibrary.simpleMessage("Slovenčina"),
+        "lastSynced":
+            MessageLookupByLibrary.simpleMessage("Posledná synchronizácia: "),
+        "leave": MessageLookupByLibrary.simpleMessage("Odísť"),
+        "link": MessageLookupByLibrary.simpleMessage("Odkaz"),
+        "logIn": MessageLookupByLibrary.simpleMessage("Prihlásiť sa"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Odhlásiť sa"),
+        "logOut2": MessageLookupByLibrary.simpleMessage("Odhlásiť sa"),
+        "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
+            "Ak chcete povoliť automatickú synchronizáciu, prihláste sa"),
+        "moreInfo": MessageLookupByLibrary.simpleMessage("Viac informácií"),
+        "newPassword": MessageLookupByLibrary.simpleMessage("Nové heslo"),
+        "nextCloud": MessageLookupByLibrary.simpleMessage("NextCloud"),
+        "notAvailable":
+            MessageLookupByLibrary.simpleMessage("Nie je k dispozícii"),
+        "noteSavedSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Uloženie poznámky úspešné"),
+        "noteUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+            "Aktualizovanie poznámky úspešné"),
+        "notesSyncSuccessfull": MessageLookupByLibrary.simpleMessage(
+            "Synchronizovanie poznámok úspešné"),
+        "notificationDescription1": MessageLookupByLibrary.simpleMessage(
+            "Nájdite si pár minút na premýšľanie o svojom dni vo svojom denníku"),
+        "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "Čas pre notifikácie nie je zvolený"),
+        "notificationTitle1":
+            MessageLookupByLibrary.simpleMessage("Čas na denník!"),
+        "notificationsNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "Notifikácie nie sú nastavené"),
+        "pageNotFound":
+            MessageLookupByLibrary.simpleMessage("Stránka nedostupná"),
+        "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
+            "Email pre resetovanie hesla bol odoslaný"),
+        "passwordResetSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Resetovanie hesla bolo úspešné"),
+        "passwordVerified":
+            MessageLookupByLibrary.simpleMessage("Heslo overené"),
+        "passwordsDontMatch":
+            MessageLookupByLibrary.simpleMessage("Heslá sa nezhodujú"),
+        "pickFromFileManager":
+            MessageLookupByLibrary.simpleMessage("Vybrať zo súborov"),
+        "pinLoginFailed": MessageLookupByLibrary.simpleMessage(
+            "Prihlásenie pomocou PIN kódu zlyhalo"),
+        "pinLoginSetupInstructions": MessageLookupByLibrary.simpleMessage(
+            "Na uzamknutej obrazovke sa zobrazí výzva na zadanie 4-miestneho kódu PIN"),
+        "pinMustBe4Digit": MessageLookupByLibrary.simpleMessage(
+            "Prosím, zadajte 4-ciferný kód PIN"),
+        "pinResetSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Resetovanie PIN kódu úspešné"),
+        "pinsDontMatch":
+            MessageLookupByLibrary.simpleMessage("Kódy PIN sa nezhodujú"),
+        "pleaseSetupYourAccountToUseThisFeature":
+            MessageLookupByLibrary.simpleMessage(
+                "Ak chcete používať túto funkciu, vytvorte si účet"),
+        "privacyPolicy": MessageLookupByLibrary.simpleMessage(
+            "Zásady ochrany osobných údajov"),
+        "projectOnGithub":
+            MessageLookupByLibrary.simpleMessage("Projekt na Githube"),
+        "recordAudio": MessageLookupByLibrary.simpleMessage("Záznam zvuku"),
+        "resetPassword":
+            MessageLookupByLibrary.simpleMessage("Resetovať heslo"),
+        "resetPin": MessageLookupByLibrary.simpleMessage("Resetovať PIN kód"),
+        "securitySettings":
+            MessageLookupByLibrary.simpleMessage("Bezpečnostné nastavenia"),
+        "sendFeedback":
+            MessageLookupByLibrary.simpleMessage("Poslať spätnú väzbu"),
+        "settings": MessageLookupByLibrary.simpleMessage("Nastavenia"),
+        "setupYourAccount":
+            MessageLookupByLibrary.simpleMessage("Konfigurácia účtu"),
+        "shareWithFriends":
+            MessageLookupByLibrary.simpleMessage("Zdielať s priateľmi"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Prihlásiť sa"),
+        "signInWithEmail":
+            MessageLookupByLibrary.simpleMessage("Prihlásenie pomocou emailu"),
+        "signUp": MessageLookupByLibrary.simpleMessage("Zaregistrovať sa"),
+        "signedInAs": MessageLookupByLibrary.simpleMessage("Prihlásený ako"),
+        "sortByAtoZ": MessageLookupByLibrary.simpleMessage("Zoradiť od A po Z"),
+        "sortByLatestFirst":
+            MessageLookupByLibrary.simpleMessage("Zoradiť podľa najnovších"),
+        "sortByOldestFirst":
+            MessageLookupByLibrary.simpleMessage("Zoradiť podľa najstarších"),
+        "stay": MessageLookupByLibrary.simpleMessage("Zostať"),
+        "submit": MessageLookupByLibrary.simpleMessage("Poslať"),
+        "syncNow": MessageLookupByLibrary.simpleMessage("Synchronizovať teraz"),
+        "tagAlreadyExists":
+            MessageLookupByLibrary.simpleMessage("Označenie už existuje"),
+        "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
+            "Ťuknutím sem rozviniete titul"),
+        "to": MessageLookupByLibrary.simpleMessage("Do"),
+        "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
+            "Príliš veľa nesprávnych pokusov, prihláste sa pomocou hesla"),
+        "unexpectedErrorOccured": MessageLookupByLibrary.simpleMessage(
+            "Vyskytla sa neočakávaná chyba"),
+        "video": MessageLookupByLibrary.simpleMessage("Video"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("WebDAV URL"),
+        "wrongPIN": MessageLookupByLibrary.simpleMessage("Nesprávny PIN kód"),
+        "youHaveUnsavedChanges":
+            MessageLookupByLibrary.simpleMessage("Máte neuložené zmeny"),
+        "youWillBeNotifiedAt": m0
+      };
+}
