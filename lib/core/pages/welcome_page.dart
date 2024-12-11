958f969203c30b97b60fcaf8a73f8cd78624eb4b
njouud <156097454+njouud@users.noreply.github.com>
2024-12-11 17:00:29 +0300

    fix image cropping in welcome screen

--- lib/core/pages/welcome_page.dart
+++ lib/core/pages/welcome_page.dart
@@ -60,38 +60,41 @@ Widget build(BuildContext context) {
         children: <Widget>[
           // Application logo
          SizedBox(
-           width: 200, 
-           height: 200, 
+           width: 150, 
+           height: 150, 
            child: ClipOval( // Crop image in oval shape to match homepage logo
-             child: Image.asset(
-              'assets/images/splash_icon_4.webp',
-              errorBuilder: (context, error, stackTrace) {
-                return const Text(
-                  'Error loading image',
-                  style: TextStyle(color: Colors.red),
-                ); 
-              },
-              fit: BoxFit.cover, // Ensures the image fills the circular frame
-              ),
-            ),
-          ), 
-          const SizedBox(height: 20),
-          // Display the random quote
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              quote,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
+            child: Transform.scale(
+              scale: 1.5, // Increase the scale to crop more from the image
+              child: Image.asset(
+                'assets/images/splash_icon_4.webp',
+                errorBuilder: (context, error, stackTrace) {
+                  return const Text(
+                    'Error loading image',
+                    style: TextStyle(color: Colors.red),
+                    ); 
+                },
+                fit: BoxFit.cover, // Ensures the image fills the circular frame
               ),
             ),
           ),
-        ],
-      ),
+        ), 
+        const SizedBox(height: 20),
+        // Display the random quote
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            quote,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+      ],
     ),
-  );
+  ),
+);
 }
 }
