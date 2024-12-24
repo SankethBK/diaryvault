bfc8352118b0f4f58f63058e4fbb66d763722b51
Geovane-Random <132626631+Geovane-Random@users.noreply.github.com>
2024-12-24 16:52:28 -0300

    Translation pt-br (#267)
    * Translationa pt-br
    
    Alterações e adição de tradução Pt-Br aos menus de configuração.
    -------------------------------------------
    Changes on Pt-Br translations of config menus.
    
    * undo wrong changes
    
    ---------
    
    Co-authored-by: Sanketh B K <sanketh.bk@razorpay.com>

--- lib/generated/intl/messages_pt.dart
+++ lib/generated/intl/messages_pt.dart
@@ -20,6 +20,8 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'pt';
 
+  static String m0(time) => "Você será notificado às ${time}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "accountSetupSuccessful": MessageLookupByLibrary.simpleMessage(
@@ -28,11 +30,15 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Já possui uma conta?"),
         "appDescription": MessageLookupByLibrary.simpleMessage(
             "Discover diaryVault - um aplicativo de diário feito para te ajudar a salvar seus pensamentos, suas memórias e seus momentos sem maiores esforços. Disponível agora na Play Store!"),
+        "appLanguage":
+            MessageLookupByLibrary.simpleMessage("Idioma do aplicativo"),
         "appVersion": MessageLookupByLibrary.simpleMessage("Versão do app"),
         "areYouSureAboutLoggingOut":
             MessageLookupByLibrary.simpleMessage("Deseja sair do aplicativo?"),
         "autoSync":
             MessageLookupByLibrary.simpleMessage("Sincronizar automaticamente"),
+        "automaticallySave": MessageLookupByLibrary.simpleMessage(
+            "Salva automaticamente suas anotações a cada 10 segundos"),
         "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
             "Sincronizar anotações para a nuvem automaticamente"),
         "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
@@ -46,12 +52,16 @@ class MessageLookup extends MessageLookupByLibrary {
         "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
             "Escolha a fonte de sincronização"),
         "chooseTheme": MessageLookupByLibrary.simpleMessage("Escolha um tema"),
+        "chooseTime": MessageLookupByLibrary.simpleMessage("Escolha a hora"),
         "closeTheApp":
             MessageLookupByLibrary.simpleMessage("Deseja fechar o app?"),
+        "cloudBackup": MessageLookupByLibrary.simpleMessage("Backup na nuvem"),
         "confirmNewPassword":
             MessageLookupByLibrary.simpleMessage("Confirmar nova senha"),
         "continueAsGues":
             MessageLookupByLibrary.simpleMessage("Continuar como convidado"),
+        "dailyReminders":
+            MessageLookupByLibrary.simpleMessage("Lembretes diários"),
         "dateFilter": MessageLookupByLibrary.simpleMessage("Filtro por data"),
         "delete": MessageLookupByLibrary.simpleMessage("Excluir"),
         "deletionFailed":
@@ -64,6 +74,8 @@ class MessageLookup extends MessageLookupByLibrary {
             "Email atualizado com sucesso, faça o login novamente"),
         "enableAutoSave": MessageLookupByLibrary.simpleMessage(
             "Habilitar salvamento automático"),
+        "enableDailyReminders":
+            MessageLookupByLibrary.simpleMessage("Ativar lembretes diários"),
         "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
             "Habilitar login por biometria"),
         "enterCurrentPassword":
@@ -72,6 +84,12 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Insira um novo email"),
         "enterRegisteredEmail":
             MessageLookupByLibrary.simpleMessage("Insira o email cadastrado"),
+        "exportNotes":
+            MessageLookupByLibrary.simpleMessage("Exporte suas notas"),
+        "exportToPDF":
+            MessageLookupByLibrary.simpleMessage("Exportar para PDF"),
+        "exportToPlainText":
+            MessageLookupByLibrary.simpleMessage("Exportar para texto simples"),
         "failedToFetchNote":
             MessageLookupByLibrary.simpleMessage("Falha ao buscar anotação"),
         "failedToSaveNote":
@@ -85,18 +103,26 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Esqueci minha senha"),
         "from": MessageLookupByLibrary.simpleMessage("De"),
         "gallery": MessageLookupByLibrary.simpleMessage("Galeria"),
+        "getDailyReminders": MessageLookupByLibrary.simpleMessage(
+            "Receba lembretes diários no horário escolhido para manter seu diário atualizado."),
         "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
+        "importAndExportNotes":
+            MessageLookupByLibrary.simpleMessage("Importar e Exportar Notas"),
         "incorrectPassword":
             MessageLookupByLibrary.simpleMessage("Senha incorreta"),
         "language": MessageLookupByLibrary.simpleMessage("Português - Brasil"),
         "lastSynced":
             MessageLookupByLibrary.simpleMessage("Última sincronização: "),
+        "leave": MessageLookupByLibrary.simpleMessage("Deixar"),
         "link": MessageLookupByLibrary.simpleMessage("Link"),
         "logIn": MessageLookupByLibrary.simpleMessage("Login"),
         "logOut": MessageLookupByLibrary.simpleMessage("Sair"),
+        "logOut2": MessageLookupByLibrary.simpleMessage("Sair"),
         "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
             "Faça o login para habilitar a sincronização automática"),
+        "moreInfo": MessageLookupByLibrary.simpleMessage("Mais informações"),
         "newPassword": MessageLookupByLibrary.simpleMessage("Nova senha"),
+        "nextCloud": MessageLookupByLibrary.simpleMessage("NextCloud"),
         "notAvailable": MessageLookupByLibrary.simpleMessage("Indisponível"),
         "noteSavedSuccessfully":
             MessageLookupByLibrary.simpleMessage("Anotação salva com sucesso"),
@@ -104,6 +130,14 @@ class MessageLookup extends MessageLookupByLibrary {
             "Anotação atualizada com sucesso"),
         "notesSyncSuccessfull": MessageLookupByLibrary.simpleMessage(
             "Anotações sincronizadas com sucesso"),
+        "notificationDescription1": MessageLookupByLibrary.simpleMessage(
+            "Reserve alguns minutos para refletir sobre o seu dia em seu diário"),
+        "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "Você não selecionou um horário de notificação"),
+        "notificationTitle1": MessageLookupByLibrary.simpleMessage(
+            "Hora de registrar no diário!"),
+        "notificationsNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "As notificações não estão ativadas"),
         "pageNotFound":
             MessageLookupByLibrary.simpleMessage("Página não encontrada"),
         "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
@@ -119,28 +153,47 @@ class MessageLookup extends MessageLookupByLibrary {
                 "Por favor, configure sua conta para usar essa funcionalidade"),
         "privacyPolicy":
             MessageLookupByLibrary.simpleMessage("Políticas de privacidade"),
+        "projectOnGithub": MessageLookupByLibrary.simpleMessage(
+            "Visite nosso projeto no Github"),
+        "reminders": MessageLookupByLibrary.simpleMessage("Lembretes"),
         "resetPassword": MessageLookupByLibrary.simpleMessage("Resetar senhar"),
+        "security": MessageLookupByLibrary.simpleMessage("Segurança"),
+        "securitySettings":
+            MessageLookupByLibrary.simpleMessage("Configurações de segurança"),
         "sendFeedback":
             MessageLookupByLibrary.simpleMessage("Mande um feedback"),
-        "settings": MessageLookupByLibrary.simpleMessage("COnfigurações"),
+        "settings": MessageLookupByLibrary.simpleMessage("Configurações"),
         "setupYourAccount":
             MessageLookupByLibrary.simpleMessage("Configure sua conta"),
-        "shareWithFriends":
-            MessageLookupByLibrary.simpleMessage("Compartilhar com os amigos"),
+        "shareWithFriends": MessageLookupByLibrary.simpleMessage(
+            "Compartilhe o app com seus amigos"),
         "signIn": MessageLookupByLibrary.simpleMessage("Entrar"),
         "signUp": MessageLookupByLibrary.simpleMessage("Cadastre-se"),
         "signedInAs": MessageLookupByLibrary.simpleMessage("Logado como"),
+        "sortByAtoZ":
+            MessageLookupByLibrary.simpleMessage("Classificar por A-Z"),
+        "sortByLatestFirst": MessageLookupByLibrary.simpleMessage(
+            "Classificar por mais recente primeiro"),
+        "sortByOldestFirst": MessageLookupByLibrary.simpleMessage(
+            "Classificar por mais antigo primeiro"),
+        "stay": MessageLookupByLibrary.simpleMessage("Ficar"),
         "submit": MessageLookupByLibrary.simpleMessage("Enviar"),
         "syncNow": MessageLookupByLibrary.simpleMessage("Sincronizar"),
+        "tagAlreadyExists":
+            MessageLookupByLibrary.simpleMessage("A etiqueta já existe"),
         "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
             "Clique aqui para expandir o título"),
+        "themeFontsAndLanguage": MessageLookupByLibrary.simpleMessage(
+            "Customize Tema, Fontes e Idioma"),
         "to": MessageLookupByLibrary.simpleMessage("Para"),
         "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
             "Muitas tentativas incorretas, tente o login utilizando a senha"),
         "unexpectedErrorOccured":
             MessageLookupByLibrary.simpleMessage("Ocorreu um erro inesperado"),
         "video": MessageLookupByLibrary.simpleMessage("Vídeo"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("URL do WebDAV"),
         "youHaveUnsavedChanges": MessageLookupByLibrary.simpleMessage(
-            "Você possui alterações não salvas")
+            "Você possui alterações não salvas"),
+        "youWillBeNotifiedAt": m0
       };
 }
