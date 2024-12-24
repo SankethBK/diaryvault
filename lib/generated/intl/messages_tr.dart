91ccc86312a83d785c3e0dd74d075c980d3861e4
Sanketh B K <sanketh.bk@razorpay.com>
2024-12-25 01:27:34 +0530

    fix tr translation

--- lib/generated/intl/messages_tr.dart
+++ lib/generated/intl/messages_tr.dart
@@ -20,7 +20,7 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'tr';
 
-  static String m0(time) => "${time} tarihinde bilgilendirileceksiniz";
+  static String m0(time) => "${time} saatinde bilgilendirileceksiniz";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
@@ -29,46 +29,47 @@ class MessageLookup extends MessageLookupByLibrary {
         "alreadyHaveAnAccount":
             MessageLookupByLibrary.simpleMessage("Zaten bir hesabınız var mı?"),
         "appDescription": MessageLookupByLibrary.simpleMessage(
-            "diaryVault\'u keşfedin - düşüncelerinizi, anılarınızı ve anlarınızı zahmetsizce yakalamanıza yardımcı olmak için tasarlanmış bir günlük uygulaması. Şimdi Play Store\'da mevcut!"),
+            "DiaryVault\'u keşfedin - düşüncelerinizi, anılarınızı ve anlarınızı zahmetsizce yakalamanıza yardımcı olmak için tasarlanmış bir günlük uygulaması. Şimdi Play Store\'da!"),
         "appLanguage": MessageLookupByLibrary.simpleMessage("Uygulama Dili"),
-        "appVersion": MessageLookupByLibrary.simpleMessage("Uygulama sürümü"),
+        "appVersion":
+            MessageLookupByLibrary.simpleMessage("Uygulama versiyonu"),
         "areYouSureAboutLoggingOut": MessageLookupByLibrary.simpleMessage(
-            "Çıkış yapmak istediğinizden emin misiniz?"),
+            "Oturumu kapatmak istediğinizden emin misiniz?"),
         "autoSync":
             MessageLookupByLibrary.simpleMessage("Otomatik senkronizasyon"),
         "automaticallySave": MessageLookupByLibrary.simpleMessage(
             "Notlarınızı her 10 saniyede bir otomatik olarak kaydeder"),
         "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
-            "Notları bulut ile otomatik olarak senkronize et"),
+            "Notları bulutla otomatik olarak senkronize edin"),
         "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
-            "Senkronizasyon için mevcut platformlar"),
+            "Senkronizasyon için kullanılabilir platformlar"),
         "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
-            "Devam ederek, şu koşulları kabul etmiş olursunuz"),
+            "Devam ederek, kabul etmiş olursunuz"),
         "camera": MessageLookupByLibrary.simpleMessage("Kamera"),
         "cancel": MessageLookupByLibrary.simpleMessage("İptal"),
-        "changeEmail":
-            MessageLookupByLibrary.simpleMessage("E-postayı değiştir"),
+        "changeEmail": MessageLookupByLibrary.simpleMessage("E-posta değiştir"),
         "changePassword":
-            MessageLookupByLibrary.simpleMessage("Şifreyi değiştir"),
+            MessageLookupByLibrary.simpleMessage("Şifre değiştir"),
         "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
-            "Senkronizasyon Kaynağını Seçin"),
-        "chooseTheme": MessageLookupByLibrary.simpleMessage("Tema seçin"),
+            "Senkronizasyon Kaynağını Seç"),
+        "chooseTheme": MessageLookupByLibrary.simpleMessage("Tema Seç"),
         "chooseTime": MessageLookupByLibrary.simpleMessage("Zaman Seçin"),
         "closeTheApp":
             MessageLookupByLibrary.simpleMessage("Uygulamayı kapat?"),
+        "cloudBackup": MessageLookupByLibrary.simpleMessage("Bulut Yedekleme"),
         "confirmNewPassword":
             MessageLookupByLibrary.simpleMessage("Yeni şifreyi onaylayın"),
         "confirmNewPin":
-            MessageLookupByLibrary.simpleMessage("Yeni PIN\'inizi onaylayın"),
+            MessageLookupByLibrary.simpleMessage("Yeni PIN\'i onaylayın"),
         "continueAsGues":
             MessageLookupByLibrary.simpleMessage("Misafir olarak devam et"),
         "dailyReminders":
-            MessageLookupByLibrary.simpleMessage("Günlük Hatırlatıcılar"),
+            MessageLookupByLibrary.simpleMessage("Günlük Hatırlatmalar"),
         "dateFilter": MessageLookupByLibrary.simpleMessage("Tarih Filtresi"),
         "delete": MessageLookupByLibrary.simpleMessage("Sil"),
         "deletionFailed":
-            MessageLookupByLibrary.simpleMessage("Silme işlemi başarısız oldu"),
-        "done": MessageLookupByLibrary.simpleMessage("Tamamlandı"),
+            MessageLookupByLibrary.simpleMessage("Silme başarısız oldu"),
+        "done": MessageLookupByLibrary.simpleMessage("Tamam"),
         "dontHaveAccount":
             MessageLookupByLibrary.simpleMessage("Hesabınız yok mu?"),
         "dropbox": MessageLookupByLibrary.simpleMessage("Dropbox"),
@@ -77,7 +78,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "enableAutoSave": MessageLookupByLibrary.simpleMessage(
             "Otomatik kaydetmeyi etkinleştir"),
         "enableDailyReminders": MessageLookupByLibrary.simpleMessage(
-            "Günlük Hatırlatıcıları Etkinleştir"),
+            "Günlük Hatırlatmaları Etkinleştir"),
         "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
             "Parmak izi ile giriş yapmayı etkinleştir"),
         "enablePINLogin": MessageLookupByLibrary.simpleMessage(
@@ -87,31 +88,33 @@ class MessageLookup extends MessageLookupByLibrary {
         "enterNewEmail":
             MessageLookupByLibrary.simpleMessage("Yeni e-posta girin"),
         "enterPin": MessageLookupByLibrary.simpleMessage("PIN\'inizi girin"),
-        "enterRegisteredEmail":
-            MessageLookupByLibrary.simpleMessage("Kayıtlı E-posta Girin"),
+        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage(
+            "Kayıtlı e-posta adresini girin"),
         "exportNotes":
-            MessageLookupByLibrary.simpleMessage("Notlarınızı dışa aktarın"),
+            MessageLookupByLibrary.simpleMessage("Notları dışa aktar"),
         "exportToPDF":
-            MessageLookupByLibrary.simpleMessage("PDF olarak dışa aktar"),
+            MessageLookupByLibrary.simpleMessage("PDF Olarak Dışa Aktar"),
         "exportToPlainText":
-            MessageLookupByLibrary.simpleMessage("Düz Metin olarak dışa aktar"),
+            MessageLookupByLibrary.simpleMessage("Düz Metin Olarak Dışa Aktar"),
         "failedToFetchNote":
             MessageLookupByLibrary.simpleMessage("Not alınamadı"),
         "failedToSaveNote":
             MessageLookupByLibrary.simpleMessage("Not kaydedilemedi"),
         "fingerPrintAthShouldBeEnabledInDeviceSettings":
             MessageLookupByLibrary.simpleMessage(
-                "Parmak izi doğrulaması cihaz ayarlarında etkinleştirilmelidir"),
+                "Parmak izi doğrulama cihaz ayarlarında etkinleştirilmelidir"),
         "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
-            "Parmak iziyle giriş başarısız oldu"),
-        "fontFamily": MessageLookupByLibrary.simpleMessage("Yazı Tipi Ailesi"),
+            "Parmak izi ile giriş başarısız"),
+        "fontFamily": MessageLookupByLibrary.simpleMessage("Yazı Tipi"),
         "forgotPassword":
-            MessageLookupByLibrary.simpleMessage("Şifremi unuttum"),
+            MessageLookupByLibrary.simpleMessage("Şifrenizi mi unuttunuz?"),
         "from": MessageLookupByLibrary.simpleMessage("Başlangıç"),
         "gallery": MessageLookupByLibrary.simpleMessage("Galeri"),
         "getDailyReminders": MessageLookupByLibrary.simpleMessage(
-            "Günlüğünüzü güncel tutmak için seçtiğiniz zamanda günlük hatırlatıcılar alın."),
+            "Günlüğünüzü güncel tutmak için seçtiğiniz saatte günlük hatırlatmalar alın."),
         "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
+        "importAndExportNotes":
+            MessageLookupByLibrary.simpleMessage("Notları İçe ve Dışa Aktar"),
         "incorrectPassword":
             MessageLookupByLibrary.simpleMessage("Hatalı şifre"),
         "language": MessageLookupByLibrary.simpleMessage("Türkçe"),
@@ -119,8 +122,8 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Son senkronizasyon: "),
         "leave": MessageLookupByLibrary.simpleMessage("Ayrıl"),
         "link": MessageLookupByLibrary.simpleMessage("Bağlantı"),
-        "logIn": MessageLookupByLibrary.simpleMessage("Giriş yap"),
-        "logOut": MessageLookupByLibrary.simpleMessage("Çıkış yap"),
+        "logIn": MessageLookupByLibrary.simpleMessage("Giriş Yap"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Çıkış Yap"),
         "logOut2": MessageLookupByLibrary.simpleMessage("Çıkış Yap"),
         "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
             "Otomatik senkronizasyonu etkinleştirmek için giriş yapın"),
@@ -139,77 +142,81 @@ class MessageLookup extends MessageLookupByLibrary {
         "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
             "Bir bildirim zamanı seçmediniz"),
         "notificationTitle1":
-            MessageLookupByLibrary.simpleMessage("Günlük Zamanı!"),
-        "notificationsNotEnabled":
-            MessageLookupByLibrary.simpleMessage("Bildirimler etkin değil"),
+            MessageLookupByLibrary.simpleMessage("Günlüğe Zaman!"),
+        "notificationsNotEnabled": MessageLookupByLibrary.simpleMessage(
+            "Bildirimler etkinleştirilmedi"),
         "pageNotFound":
             MessageLookupByLibrary.simpleMessage("Sayfa bulunamadı"),
         "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
             "Şifre sıfırlama e-postası gönderildi"),
         "passwordResetSuccessful":
-            MessageLookupByLibrary.simpleMessage("Şifre başarıyla sıfırlandı"),
+            MessageLookupByLibrary.simpleMessage("Şifre sıfırlama başarılı"),
         "passwordVerified":
             MessageLookupByLibrary.simpleMessage("Şifre doğrulandı"),
         "passwordsDontMatch":
-            MessageLookupByLibrary.simpleMessage("Şifreler uyuşmuyor"),
+            MessageLookupByLibrary.simpleMessage("Şifreler eşleşmiyor"),
         "pickFromFileManager":
-            MessageLookupByLibrary.simpleMessage("Dosya Yöneticisinden Seç"),
+            MessageLookupByLibrary.simpleMessage("Dosyalardan Seç"),
         "pinLoginFailed": MessageLookupByLibrary.simpleMessage(
             "PIN ile giriş başarısız oldu"),
         "pinLoginSetupInstructions": MessageLookupByLibrary.simpleMessage(
-            "Kilit ekranında 4 haneli bir PIN sorulacaktır"),
+            "Ekran kilidinde 4 haneli bir PIN sorulacaktır"),
         "pinMustBe4Digit": MessageLookupByLibrary.simpleMessage(
             "Lütfen 4 haneli bir PIN girin"),
         "pinResetSuccessful":
-            MessageLookupByLibrary.simpleMessage("PIN sıfırlama başarılı"),
+            MessageLookupByLibrary.simpleMessage("PIN başarıyla sıfırlandı"),
         "pinsDontMatch":
-            MessageLookupByLibrary.simpleMessage("PIN\'ler uyuşmuyor"),
+            MessageLookupByLibrary.simpleMessage("PIN\'ler eşleşmiyor"),
         "pleaseSetupYourAccountToUseThisFeature":
             MessageLookupByLibrary.simpleMessage(
-                "Bu özelliği kullanmak için lütfen hesabınızı kurun"),
+                "Bu özelliği kullanmak için lütfen hesabınızı ayarlayın"),
         "privacyPolicy":
             MessageLookupByLibrary.simpleMessage("Gizlilik Politikası"),
         "projectOnGithub":
-            MessageLookupByLibrary.simpleMessage("Github\'da Proje"),
+            MessageLookupByLibrary.simpleMessage("Proje Github\'da"),
         "recordAudio": MessageLookupByLibrary.simpleMessage("Ses Kaydet"),
-        "resetPassword":
-            MessageLookupByLibrary.simpleMessage("Şifreyi sıfırla"),
+        "reminders": MessageLookupByLibrary.simpleMessage("Hatırlatmalar"),
+        "resetPassword": MessageLookupByLibrary.simpleMessage("Şifre sıfırla"),
         "resetPin": MessageLookupByLibrary.simpleMessage("PIN Sıfırla"),
+        "security": MessageLookupByLibrary.simpleMessage("Güvenlik"),
         "securitySettings":
             MessageLookupByLibrary.simpleMessage("Güvenlik Ayarları"),
         "sendFeedback":
             MessageLookupByLibrary.simpleMessage("Geri bildirim gönder"),
         "settings": MessageLookupByLibrary.simpleMessage("Ayarlar"),
         "setupYourAccount":
-            MessageLookupByLibrary.simpleMessage("Hesabınızı Kurun"),
+            MessageLookupByLibrary.simpleMessage("Hesabınızı Ayarlayın"),
         "shareWithFriends":
-            MessageLookupByLibrary.simpleMessage("Arkadaşlarınızla paylaşın"),
-        "signIn": MessageLookupByLibrary.simpleMessage("Oturum aç"),
+            MessageLookupByLibrary.simpleMessage("Arkadaşlarla paylaş"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Giriş Yap"),
         "signInWithEmail":
             MessageLookupByLibrary.simpleMessage("E-posta ile giriş yap"),
-        "signUp": MessageLookupByLibrary.simpleMessage("Kaydol"),
-        "signedInAs": MessageLookupByLibrary.simpleMessage("Giriş yapıldı: "),
+        "signUp": MessageLookupByLibrary.simpleMessage("Kayıt Ol"),
+        "signedInAs":
+            MessageLookupByLibrary.simpleMessage("Olarak giriş yapıldı"),
         "sortByAtoZ":
             MessageLookupByLibrary.simpleMessage("A\'dan Z\'ye Sırala"),
-        "sortByLatestFirst":
-            MessageLookupByLibrary.simpleMessage("En Yeniden En Eskiye Sırala"),
-        "sortByOldestFirst":
-            MessageLookupByLibrary.simpleMessage("En Eskiden En Yeniye Sırala"),
+        "sortByLatestFirst": MessageLookupByLibrary.simpleMessage(
+            "En Yeniden Başlayarak Sırala"),
+        "sortByOldestFirst": MessageLookupByLibrary.simpleMessage(
+            "En Eskiden Başlayarak Sırala"),
         "stay": MessageLookupByLibrary.simpleMessage("Kal"),
         "submit": MessageLookupByLibrary.simpleMessage("Gönder"),
         "syncNow": MessageLookupByLibrary.simpleMessage("Şimdi senkronize et"),
         "tagAlreadyExists":
             MessageLookupByLibrary.simpleMessage("Etiket zaten mevcut"),
         "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
-            "Başlığı genişletmek için buraya dokunun"),
+            "Başlığı genişletmek için dokunun"),
+        "themeFontsAndLanguage": MessageLookupByLibrary.simpleMessage(
+            "Temayı, Yazı Tiplerini ve Dili Özelleştir"),
         "to": MessageLookupByLibrary.simpleMessage("Bitiş"),
         "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
-            "Çok fazla yanlış deneme yapıldı, lütfen şifrenizle giriş yapın"),
+            "Çok fazla hatalı deneme, lütfen şifre ile giriş yapın"),
         "unexpectedErrorOccured":
-            MessageLookupByLibrary.simpleMessage("Beklenmedik bir hata oluştu"),
+            MessageLookupByLibrary.simpleMessage("Beklenmeyen bir hata oluştu"),
         "video": MessageLookupByLibrary.simpleMessage("Video"),
-        "webdavURL": MessageLookupByLibrary.simpleMessage("WebDAV URL"),
-        "wrongPIN": MessageLookupByLibrary.simpleMessage("Yanlış PIN"),
+        "webdavURL": MessageLookupByLibrary.simpleMessage("WebDAV URL\'si"),
+        "wrongPIN": MessageLookupByLibrary.simpleMessage("Hatalı PIN"),
         "youHaveUnsavedChanges": MessageLookupByLibrary.simpleMessage(
             "Kaydedilmemiş değişiklikleriniz var"),
         "youWillBeNotifiedAt": m0
