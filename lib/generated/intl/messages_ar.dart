e83442264a5cb6888678dde1ddb343235dbf647a
Med <106492427+MrMDrX@users.noreply.github.com>
2025-09-10 09:32:43 +0000

    💫 completed missing arabic translation (#296)

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -20,124 +20,254 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ar';
 
+  static String m0(time) => "سيتم إعلامك في ${time}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "accountSetupSuccessful":
-            MessageLookupByLibrary.simpleMessage("تم إعداد الحساب بنجاح"),
-        "alreadyHaveAnAccount":
-            MessageLookupByLibrary.simpleMessage("هل لديك حساب بالفعل؟"),
-        "appDescription": MessageLookupByLibrary.simpleMessage(
-            "اكتشف diaryVault - تطبيق مذكرات مصمم لمساعدتك في التقاط أفكارك وذكرياتك ولحظاتك بسهولة. متوفر الآن على متجر Google Play!"),
-        "appVersion": MessageLookupByLibrary.simpleMessage("إصدار التطبيق"),
-        "areYouSureAboutLoggingOut": MessageLookupByLibrary.simpleMessage(
-            "هل أنت متأكد من تسجيل الخروج؟"),
-        "autoSync": MessageLookupByLibrary.simpleMessage("مزامنة تلقائية"),
-        "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
-            "مزامنة الملاحظات تلقائيًا مع السحابة"),
-        "availablePlatformsForSync":
-            MessageLookupByLibrary.simpleMessage("المنصات المتاحة للمزامنة"),
-        "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
-            "من خلال المتابعة، أنت توافق على"),
-        "camera": MessageLookupByLibrary.simpleMessage("الكاميرا"),
-        "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
-        "changeEmail":
-            MessageLookupByLibrary.simpleMessage("تغيير البريد الإلكتروني"),
-        "changePassword":
-            MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
-        "chooseTheSyncSource":
-            MessageLookupByLibrary.simpleMessage("اختر مصدر المزامنة"),
-        "chooseTheme": MessageLookupByLibrary.simpleMessage("اختر السمة"),
-        "closeTheApp":
-            MessageLookupByLibrary.simpleMessage("هل تريد إغلاق التطبيق؟"),
-        "confirmNewPassword":
-            MessageLookupByLibrary.simpleMessage("تأكيد كلمة المرور الجديدة"),
-        "continueAsGues": MessageLookupByLibrary.simpleMessage("المتابعة كضيف"),
-        "dateFilter": MessageLookupByLibrary.simpleMessage("تصفية التاريخ"),
-        "delete": MessageLookupByLibrary.simpleMessage("حذف"),
-        "deletionFailed": MessageLookupByLibrary.simpleMessage("فشل الحذف"),
-        "done": MessageLookupByLibrary.simpleMessage("تم"),
-        "dontHaveAccount":
-            MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟"),
-        "dropbox": MessageLookupByLibrary.simpleMessage("دروب بوكس"),
-        "emailUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
-            "تم تحديث البريد الإلكتروني بنجاح، يرجى تسجيل الدخول مرة أخرى"),
-        "enableAutoSave":
-            MessageLookupByLibrary.simpleMessage("تمكين الحفظ التلقائي"),
-        "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
-            "تمكين تسجيل الدخول ببصمة الإصبع"),
-        "enterCurrentPassword":
-            MessageLookupByLibrary.simpleMessage("أدخل كلمة المرور الحالية"),
-        "enterNewEmail":
-            MessageLookupByLibrary.simpleMessage("أدخل بريد إلكتروني جديد"),
-        "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage(
-            "أدخل البريد الإلكتروني المسجل"),
-        "failedToFetchNote":
-            MessageLookupByLibrary.simpleMessage("فشل جلب المذكرة"),
-        "failedToSaveNote":
-            MessageLookupByLibrary.simpleMessage("فشل حفظ المذكرة"),
-        "fingerPrintAthShouldBeEnabledInDeviceSettings":
-            MessageLookupByLibrary.simpleMessage(
-                "يجب تمكين تسجيل الدخول ببصمة الإصبع في إعدادات الجهاز"),
-        "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
-            "فشل تسجيل الدخول ببصمة الإصبع"),
-        "forgotPassword":
-            MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور"),
-        "from": MessageLookupByLibrary.simpleMessage("من"),
-        "gallery": MessageLookupByLibrary.simpleMessage("المعرض"),
-        "googleDrive": MessageLookupByLibrary.simpleMessage("جوجل درايف"),
-        "incorrectPassword":
-            MessageLookupByLibrary.simpleMessage("كلمة المرور غير صحيحة"),
-        "language": MessageLookupByLibrary.simpleMessage("العربية"),
-        "lastSynced": MessageLookupByLibrary.simpleMessage("آخر مزامنة: "),
-        "link": MessageLookupByLibrary.simpleMessage("رابط"),
-        "logIn": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
-        "logOut": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
-        "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
-            "الرجاء تسجيل الدخول لتمكين المزامنة التلقائية"),
-        "newPassword": MessageLookupByLibrary.simpleMessage("كلمة مرور جديدة"),
-        "notAvailable": MessageLookupByLibrary.simpleMessage("غير متاح"),
-        "noteSavedSuccessfully":
-            MessageLookupByLibrary.simpleMessage("تم حفظ المذكرة بنجاح"),
-        "noteUpdatedSuccessfully":
-            MessageLookupByLibrary.simpleMessage("تم تحديث المذكرة بنجاح"),
-        "notesSyncSuccessfull":
-            MessageLookupByLibrary.simpleMessage("مزامنة الملاحظات ناجحة"),
-        "pageNotFound":
-            MessageLookupByLibrary.simpleMessage("الصفحة غير موجودة"),
-        "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
-            "تم إرسال بريد إعادة تعيين كلمة المرور"),
-        "passwordResetSuccessful": MessageLookupByLibrary.simpleMessage(
-            "إعادة تعيين كلمة المرور بنجاح"),
-        "passwordVerified":
-            MessageLookupByLibrary.simpleMessage("تم التحقق من كلمة المرور"),
-        "passwordsDontMatch":
-            MessageLookupByLibrary.simpleMessage("كلمات المرور غير متطابقة"),
-        "pleaseSetupYourAccountToUseThisFeature":
-            MessageLookupByLibrary.simpleMessage(
-                "يرجى إعداد حسابك لاستخدام هذه الميزة"),
-        "privacyPolicy": MessageLookupByLibrary.simpleMessage("سياسة الخصوصية"),
-        "resetPassword":
-            MessageLookupByLibrary.simpleMessage("إعادة تعيين كلمة المرور"),
-        "sendFeedback": MessageLookupByLibrary.simpleMessage("إرسال ملاحظات"),
-        "settings": MessageLookupByLibrary.simpleMessage("الإعدادات"),
-        "setupYourAccount": MessageLookupByLibrary.simpleMessage("إعداد حسابك"),
-        "shareWithFriends":
-            MessageLookupByLibrary.simpleMessage("مشاركة مع الأصدقاء"),
-        "signIn": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
-        "signUp": MessageLookupByLibrary.simpleMessage("التسجيل"),
-        "signedInAs":
-            MessageLookupByLibrary.simpleMessage("تم تسجيل الدخول باسم"),
-        "submit": MessageLookupByLibrary.simpleMessage("إرسال"),
-        "syncNow": MessageLookupByLibrary.simpleMessage("مزامنة الآن"),
-        "tapToExpandTitle":
-            MessageLookupByLibrary.simpleMessage("اضغط هنا لتوسيع العنوان"),
-        "to": MessageLookupByLibrary.simpleMessage("إلى"),
-        "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
-            "عدد كبير من المحاولات الخاطئة، يرجى تسجيل الدخول بكلمة المرور"),
-        "unexpectedErrorOccured":
-            MessageLookupByLibrary.simpleMessage("حدث خطأ غير متوقع"),
-        "video": MessageLookupByLibrary.simpleMessage("فيديو"),
-        "youHaveUnsavedChanges":
-            MessageLookupByLibrary.simpleMessage("لديك تغييرات غير محفوظة")
-      };
+    "accountSetupSuccessful": MessageLookupByLibrary.simpleMessage(
+      "تم إعداد الحساب بنجاح",
+    ),
+    "alreadyHaveAnAccount": MessageLookupByLibrary.simpleMessage(
+      "هل لديك حساب بالفعل؟",
+    ),
+    "appDescription": MessageLookupByLibrary.simpleMessage(
+      "اكتشف diaryVault - تطبيق مذكرات مصمم لمساعدتك في التقاط أفكارك وذكرياتك ولحظاتك بسهولة. متوفر الآن على متجر Google Play!",
+    ),
+    "appLanguage": MessageLookupByLibrary.simpleMessage("لغة التطبيق"),
+    "appVersion": MessageLookupByLibrary.simpleMessage("إصدار التطبيق"),
+    "areYouSureAboutLoggingOut": MessageLookupByLibrary.simpleMessage(
+      "هل أنت متأكد من تسجيل الخروج؟",
+    ),
+    "autoSync": MessageLookupByLibrary.simpleMessage("مزامنة تلقائية"),
+    "automaticallySave": MessageLookupByLibrary.simpleMessage(
+      "حفظ الملاحظات تلقائيًا كل 10 ثوانٍ",
+    ),
+    "automaticallySyncNotesWithCloud": MessageLookupByLibrary.simpleMessage(
+      "مزامنة الملاحظات تلقائيًا مع السحابة",
+    ),
+    "availablePlatformsForSync": MessageLookupByLibrary.simpleMessage(
+      "المنصات المتاحة للمزامنة",
+    ),
+    "byContinuingYouAgree": MessageLookupByLibrary.simpleMessage(
+      "من خلال المتابعة، أنت توافق على",
+    ),
+    "camera": MessageLookupByLibrary.simpleMessage("الكاميرا"),
+    "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+    "changeEmail": MessageLookupByLibrary.simpleMessage(
+      "تغيير البريد الإلكتروني",
+    ),
+    "changePassword": MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
+    "chooseTheSyncSource": MessageLookupByLibrary.simpleMessage(
+      "اختر مصدر المزامنة",
+    ),
+    "chooseTheme": MessageLookupByLibrary.simpleMessage("اختر السمة"),
+    "chooseTime": MessageLookupByLibrary.simpleMessage("اختر الوقت"),
+    "closeTheApp": MessageLookupByLibrary.simpleMessage(
+      "هل تريد إغلاق التطبيق؟",
+    ),
+    "cloudBackup": MessageLookupByLibrary.simpleMessage(
+      "نسخة احتياطية على السحابة",
+    ),
+    "confirmNewPassword": MessageLookupByLibrary.simpleMessage(
+      "تأكيد كلمة المرور الجديدة",
+    ),
+    "confirmNewPin": MessageLookupByLibrary.simpleMessage(
+      "تأكيد الرقم السري الجديد",
+    ),
+    "continueAsGues": MessageLookupByLibrary.simpleMessage("المتابعة كضيف"),
+    "dailyReminders": MessageLookupByLibrary.simpleMessage("تذكيرات يومية"),
+    "dateFilter": MessageLookupByLibrary.simpleMessage("التصفية حسب التاريخ"),
+    "delete": MessageLookupByLibrary.simpleMessage("حذف"),
+    "deletionFailed": MessageLookupByLibrary.simpleMessage("فشل الحذف"),
+    "done": MessageLookupByLibrary.simpleMessage("تم"),
+    "dontHaveAccount": MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟"),
+    "dropbox": MessageLookupByLibrary.simpleMessage("دروب بوكس"),
+    "emailUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم تحديث البريد الإلكتروني بنجاح، يرجى تسجيل الدخول مرة أخرى",
+    ),
+    "enableAutoSave": MessageLookupByLibrary.simpleMessage(
+      "تفعيل الحفظ التلقائي",
+    ),
+    "enableDailyReminders": MessageLookupByLibrary.simpleMessage(
+      "تفعيل التذكيرات اليومية",
+    ),
+    "enableFingerPrintLogin": MessageLookupByLibrary.simpleMessage(
+      "تفعيل تسجيل الدخول ببصمة الإصبع",
+    ),
+    "enablePINLogin": MessageLookupByLibrary.simpleMessage(
+      "تفعيل تسجيل الدخول بالرقم السري",
+    ),
+    "enterCurrentPassword": MessageLookupByLibrary.simpleMessage(
+      "أدخل كلمة المرور الحالية",
+    ),
+    "enterNewEmail": MessageLookupByLibrary.simpleMessage(
+      "أدخل بريد إلكتروني جديد",
+    ),
+    "enterPin": MessageLookupByLibrary.simpleMessage("أدخل الرقم السري"),
+    "enterRegisteredEmail": MessageLookupByLibrary.simpleMessage(
+      "أدخل البريد الإلكتروني المسجل",
+    ),
+    "exportNotes": MessageLookupByLibrary.simpleMessage("تصدير الملاحظات"),
+    "exportToJSON": MessageLookupByLibrary.simpleMessage("تصدير إلى JSON"),
+    "exportToPDF": MessageLookupByLibrary.simpleMessage("تصدير إلى PDF"),
+    "exportToPlainText": MessageLookupByLibrary.simpleMessage("تصدير كنص عادي"),
+    "failedToFetchNote": MessageLookupByLibrary.simpleMessage(
+      "فشل جلب المذكرة",
+    ),
+    "failedToSaveNote": MessageLookupByLibrary.simpleMessage("فشل حفظ المذكرة"),
+    "fingerPrintAthShouldBeEnabledInDeviceSettings":
+        MessageLookupByLibrary.simpleMessage(
+          "يجب تفعيل تسجيل الدخول ببصمة الإصبع في إعدادات الجهاز",
+        ),
+    "fingerprintLoginFailed": MessageLookupByLibrary.simpleMessage(
+      "فشل تسجيل الدخول ببصمة الإصبع",
+    ),
+    "fontFamily": MessageLookupByLibrary.simpleMessage("نوع الخط"),
+    "forgotPassword": MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور"),
+    "from": MessageLookupByLibrary.simpleMessage("من"),
+    "gallery": MessageLookupByLibrary.simpleMessage("المعرض"),
+    "getDailyReminders": MessageLookupByLibrary.simpleMessage(
+      "احصل على تذكيرات يومية في الوقت الذي تختاره لتحديث مذكرتك.",
+    ),
+    "googleDrive": MessageLookupByLibrary.simpleMessage("جوجل درايف"),
+    "importAndExportNotes": MessageLookupByLibrary.simpleMessage(
+      "استيراد وتصدير الملاحظات",
+    ),
+    "incorrectPassword": MessageLookupByLibrary.simpleMessage(
+      "كلمة المرور غير صحيحة",
+    ),
+    "language": MessageLookupByLibrary.simpleMessage("العربية"),
+    "lastSynced": MessageLookupByLibrary.simpleMessage("آخر مزامنة: "),
+    "leave": MessageLookupByLibrary.simpleMessage("مغادرة"),
+    "link": MessageLookupByLibrary.simpleMessage("رابط"),
+    "logIn": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+    "logOut": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+    "logOut2": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+    "loginToEnableAutoSync": MessageLookupByLibrary.simpleMessage(
+      "الرجاء تسجيل الدخول لتفعيل المزامنة التلقائية",
+    ),
+    "moreInfo": MessageLookupByLibrary.simpleMessage("المزيد من المعلومات"),
+    "newPassword": MessageLookupByLibrary.simpleMessage("كلمة مرور جديدة"),
+    "nextCloud": MessageLookupByLibrary.simpleMessage("NextCloud"),
+    "notAvailable": MessageLookupByLibrary.simpleMessage("غير متاح"),
+    "noteSavedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم حفظ المذكرة بنجاح",
+    ),
+    "noteUpdatedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم تحديث المذكرة بنجاح",
+    ),
+    "notesSyncSuccessfull": MessageLookupByLibrary.simpleMessage(
+      "تمّ مزامنة الملاحظات بنجاح",
+    ),
+    "notificationDescription1": MessageLookupByLibrary.simpleMessage(
+      "خذ بضع دقائق لتدوين يومك في مذكرتك",
+    ),
+    "notificationTimeNotEnabled": MessageLookupByLibrary.simpleMessage(
+      "لم تقم بتحديد وقت للإشعار",
+    ),
+    "notificationTitle1": MessageLookupByLibrary.simpleMessage(
+      "حان وقت كتابة المذكرة!",
+    ),
+    "notificationsNotEnabled": MessageLookupByLibrary.simpleMessage(
+      "الإشعارات غير مفعلة",
+    ),
+    "pageNotFound": MessageLookupByLibrary.simpleMessage("الصفحة غير موجودة"),
+    "passwordResetMailSent": MessageLookupByLibrary.simpleMessage(
+      "تم إرسال بريد إعادة تعيين كلمة المرور",
+    ),
+    "passwordResetSuccessful": MessageLookupByLibrary.simpleMessage(
+      "تمّ إعادة تعيين كلمة المرور بنجاح",
+    ),
+    "passwordVerified": MessageLookupByLibrary.simpleMessage(
+      "تم التحقق من كلمة المرور",
+    ),
+    "passwordsDontMatch": MessageLookupByLibrary.simpleMessage(
+      "كلمات المرور غير متطابقة",
+    ),
+    "pickFromFileManager": MessageLookupByLibrary.simpleMessage(
+      "اختر من الملفات",
+    ),
+    "pinLoginFailed": MessageLookupByLibrary.simpleMessage(
+      "فشل تسجيل الدخول بالرقم السري",
+    ),
+    "pinLoginSetupInstructions": MessageLookupByLibrary.simpleMessage(
+      "سيُطلب منك إدخال رقم سري من 4 أرقام في شاشة القفل",
+    ),
+    "pinMustBe4Digit": MessageLookupByLibrary.simpleMessage(
+      "يرجى إدخال رقم سري مكون من 4 أرقام",
+    ),
+    "pinResetSuccessful": MessageLookupByLibrary.simpleMessage(
+      "تمّ إعادة تعيين رقمك السري بنجاح",
+    ),
+    "pinsDontMatch": MessageLookupByLibrary.simpleMessage(
+      "الأرقام السرية غير متطابقة",
+    ),
+    "pleaseSetupYourAccountToUseThisFeature":
+        MessageLookupByLibrary.simpleMessage(
+          "يرجى إعداد حسابك لاستخدام هذه الميزة",
+        ),
+    "privacyPolicy": MessageLookupByLibrary.simpleMessage("سياسة الخصوصية"),
+    "projectOnGithub": MessageLookupByLibrary.simpleMessage(
+      "المشروع على GitHub",
+    ),
+    "recordAudio": MessageLookupByLibrary.simpleMessage("تسجيل صوتي"),
+    "reminders": MessageLookupByLibrary.simpleMessage("التذكيرات"),
+    "resetPassword": MessageLookupByLibrary.simpleMessage(
+      "إعادة تعيين كلمة المرور",
+    ),
+    "resetPin": MessageLookupByLibrary.simpleMessage("إعادة تعيين الرقم السري"),
+    "security": MessageLookupByLibrary.simpleMessage("الأمان"),
+    "securitySettings": MessageLookupByLibrary.simpleMessage("إعدادات الأمان"),
+    "selectVoice": MessageLookupByLibrary.simpleMessage("اختر الصوت"),
+    "sendFeedback": MessageLookupByLibrary.simpleMessage("إرسال ملاحظات"),
+    "settings": MessageLookupByLibrary.simpleMessage("الإعدادات"),
+    "setupYourAccount": MessageLookupByLibrary.simpleMessage("إعداد حسابك"),
+    "shareWithFriends": MessageLookupByLibrary.simpleMessage(
+      "مشاركة مع الأصدقاء",
+    ),
+    "signIn": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+    "signInWithEmail": MessageLookupByLibrary.simpleMessage(
+      "تسجيل الدخول بالبريد الإلكتروني",
+    ),
+    "signUp": MessageLookupByLibrary.simpleMessage("التسجيل"),
+    "signedInAs": MessageLookupByLibrary.simpleMessage("تم تسجيل الدخول باسم"),
+    "sortByAtoZ": MessageLookupByLibrary.simpleMessage(
+      "الفرز أبجدياً من A إلى Z",
+    ),
+    "sortByLatestFirst": MessageLookupByLibrary.simpleMessage(
+      "الفرز من الأحدث إلى الأقدم",
+    ),
+    "sortByOldestFirst": MessageLookupByLibrary.simpleMessage(
+      "الفرز من الأقدم إلى الأحدث",
+    ),
+    "stay": MessageLookupByLibrary.simpleMessage("البقاء"),
+    "submit": MessageLookupByLibrary.simpleMessage("إرسال"),
+    "syncNow": MessageLookupByLibrary.simpleMessage("مزامنة الآن"),
+    "tagAlreadyExists": MessageLookupByLibrary.simpleMessage(
+      "الوسم موجود بالفعل",
+    ),
+    "tapToExpandTitle": MessageLookupByLibrary.simpleMessage(
+      "اضغط هنا لتوسيع العنوان",
+    ),
+    "themeFontsAndLanguage": MessageLookupByLibrary.simpleMessage(
+      "تخصيص السمة والخط واللغة",
+    ),
+    "to": MessageLookupByLibrary.simpleMessage("إلى"),
+    "tooManyWrongAttempts": MessageLookupByLibrary.simpleMessage(
+      "عدد كبير من المحاولات الخاطئة، يرجى تسجيل الدخول بكلمة المرور",
+    ),
+    "toolbarPosition": MessageLookupByLibrary.simpleMessage(
+      "موضع شريط الأدوات",
+    ),
+    "unexpectedErrorOccured": MessageLookupByLibrary.simpleMessage(
+      "حدث خطأ غير متوقع",
+    ),
+    "video": MessageLookupByLibrary.simpleMessage("فيديو"),
+    "webdavURL": MessageLookupByLibrary.simpleMessage("رابط WebDAV"),
+    "wrongPIN": MessageLookupByLibrary.simpleMessage("رقم سري خاطئ"),
+    "youHaveUnsavedChanges": MessageLookupByLibrary.simpleMessage(
+      "لديك تعديلات غير محفوظة",
+    ),
+    "youWillBeNotifiedAt": m0,
+  };
 }
